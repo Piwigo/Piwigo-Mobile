827ec8e9e0fdf8d74dc3a665843a3a75644e593c
Spencer Baker <spencer@bakercrew.com>
2015-01-20 23:34:01 -0700

    Added tab bar icons.

--- piwigo/TabBarViewController.m
+++ piwigo/TabBarViewController.m
@@ -27,12 +27,18 @@
 		
 		AlbumsViewController *albums = [AlbumsViewController new];
 		albums.title = @"Albums";
+		albums.tabBarItem.image = [UIImage imageNamed:@"album"];
+		albums.tabBarItem.selectedImage = [UIImage imageNamed:@"albumSelected"];
 		
 		UploadViewController *upload = [UploadViewController new];
 		upload.title = @"Upload";
+		upload.tabBarItem.image = [UIImage imageNamed:@"cloud"];
+		upload.tabBarItem.selectedImage = [UIImage imageNamed:@"cloudSelected"];
 		
 		SettingsViewController *settings = [SettingsViewController new];
 		settings.title = @"Settings";
+		settings.tabBarItem.image = [UIImage imageNamed:@"preferences"];
+		settings.tabBarItem.selectedImage = [UIImage imageNamed:@"preferencesSelected"];
 		
 		self.viewControllers = @[albums, upload, settings];
 	}
