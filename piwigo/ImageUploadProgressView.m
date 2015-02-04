50140c4ac46025a828ca32f980508e8b23668abb
Spencer Baker <spencer@bakercrew.com>
2015-02-04 11:43:45 -0700

    Added a progress bar for uploading images.

--- piwigo/ImageUploadProgressView.m
+++ piwigo/ImageUploadProgressView.m
@@ -13,9 +13,11 @@
 @interface ImageUploadProgressView() <ImageUploadDelegate>
 
 @property (nonatomic, strong) UILabel *imageCountLabel;
+@property (nonatomic, strong) UIProgressView *uploadProgress;
 
 @property (nonatomic, assign) NSInteger currentImage;
 @property (nonatomic, assign) NSInteger maxImages;
+
 @end
 
 @implementation ImageUploadProgressView
@@ -38,7 +40,17 @@
 		self.imageCountLabel.text = @"Uploading 0/0";
 		[self addSubview:self.imageCountLabel];
 		[self addConstraint:[NSLayoutConstraint constraintVerticalCenterView:self.imageCountLabel]];
-		[self addConstraint:[NSLayoutConstraint constrainViewFromLeft:self.imageCountLabel amount:15]];
+		
+		self.uploadProgress = [[UIProgressView alloc] init];
+		self.uploadProgress.translatesAutoresizingMaskIntoConstraints = NO;
+		[self addSubview:self.uploadProgress];
+		[self addConstraint:[NSLayoutConstraint constraintVerticalCenterView:self.uploadProgress]];
+		
+		[self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"|-15-[label]-10-[progress]-15-|"
+																	 options:kNilOptions
+																	 metrics:nil
+																	   views:@{@"label" : self.imageCountLabel,
+																			   @"progress" : self.uploadProgress}]];
 		
 	}
 	return self;
@@ -48,7 +60,15 @@
 
 -(void)imageProgress:(ImageUpload *)image onCurrent:(NSInteger)current forTotal:(NSInteger)total onChunk:(NSInteger)currentChunk forChunks:(NSInteger)totalChunks
 {
-	
+	CGFloat chunkPercent = 100.0 / totalChunks / 100.0;
+	CGFloat onChunkPercent = chunkPercent * (currentChunk - 1);
+	CGFloat peiceProgress = (CGFloat)current / total;
+	CGFloat totalProgress = onChunkPercent + (chunkPercent * peiceProgress);
+	if(totalProgress > 1)
+	{
+		totalProgress = 1;
+	}
+	[self.uploadProgress setProgress:totalProgress animated:YES];
 }
 
 -(void)imageUploaded:(ImageUpload *)image placeInQueue:(NSInteger)rank outOf:(NSInteger)totalInQueue
@@ -59,6 +79,7 @@
 		self.currentImage = totalInQueue;
 	}
 	[self updateImageCountLabel];
+	[self.uploadProgress setProgress:0 animated:NO];
 }
 
 -(void)imagesToUploadChanged:(NSInteger)imagesLeftToUpload
