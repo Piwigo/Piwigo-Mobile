6335c56992dfb064781eb9e04fc9be3fd95d5e11
Olaf Greck <olaf@greck.eu>
2015-04-13 12:19:00 +0200

    iRate added

--- piwigo/AppDelegate.m
+++ piwigo/AppDelegate.m
@@ -17,6 +17,7 @@
 #import "AFNetworkActivityIndicatorManager.h"
 
 #import "PhotosFetch.h"
+#import "iRate.h"
 
 @interface AppDelegate ()
 
@@ -25,6 +26,17 @@
 @end
 
 @implementation AppDelegate
+
++ (void)initialize {
+    //configure iRate
+    [iRate sharedInstance].appStoreID       = 472225196;
+    [iRate sharedInstance].daysUntilPrompt  = 5;
+    [iRate sharedInstance].usesUntilPrompt  = 5;
+    [iRate sharedInstance].promptForNewVersionIfUserRated = YES;
+    [iRate sharedInstance].promptAtLaunch   = NO;
+//#warning Preview mode
+//    [iRate sharedInstance].previewMode      = YES;
+}
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
