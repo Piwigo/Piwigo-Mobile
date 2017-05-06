b3d2395bfc1aca56106a688e0473836be1c63a73
Eddy Lelièvre-Berna <eddy@lelievre-berna.net>
2017-05-06 18:30:30 +0200

    TabBar folder
    TabBar related files moved into TabBar folder

--- piwigo/AppDelegate.m
+++ piwigo/AppDelegate.m
@@ -10,7 +10,7 @@
 #import "LoginNavigationController.h"
 #import "LoginViewController_iPhone.h"
 #import "LoginViewController_iPad.h"
-#import "TabBarViewController.h"
+#import "TabBar/TabBarViewController.h"
 #import "SessionService.h"
 #import "Model.h"
 #import "KeychainAccess.h"
