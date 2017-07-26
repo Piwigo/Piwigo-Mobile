0cca64104e82c8bd3f8f53023debae48548f7eec
Eddy Lelièvre-Berna <eddy@lelievre-berna.net>
2017-07-26 19:18:54 +0200

    Updated release notes

--- piwigo/Settings/Release Notes/ReleaseNotesViewController.m
+++ piwigo/Settings/Release Notes/ReleaseNotesViewController.m
@@ -51,75 +51,75 @@
         // Release notes string
         NSString *notesString = @"";
         
-            // Release 2.0.4 — Bundle string
-            NSString *v204String = NSLocalizedStringFromTableInBundle(@"v2.0.4_text", @"ReleaseNotes", [NSBundle mainBundle], @"v2.0.4 Release Notes text");
-            notesString = [notesString stringByAppendingString:v204String];
-
-            // Release 2.0.3 — Bundle string
-            NSString *v203String = NSLocalizedStringFromTableInBundle(@"v2.0.3_text", @"ReleaseNotes", [NSBundle mainBundle], @"v2.0.3 Release Notes text");
-            notesString = [notesString stringByAppendingString:v203String];
-            
-            // Release 2.0.2 — Bundle string
-            NSString *v202String = NSLocalizedStringFromTableInBundle(@"v2.0.2_text", @"ReleaseNotes", [NSBundle mainBundle], @"v2.0.2 Release Notes text");
-            notesString = [notesString stringByAppendingString:v202String];
-
-            // Release 2.0.1 — Bundle string
-            NSString *v201String = NSLocalizedStringFromTableInBundle(@"v2.0.1_text", @"ReleaseNotes", [NSBundle mainBundle], @"v2.0.1 Release Notes text");
-            notesString = [notesString stringByAppendingString:v201String];
-            
-            // Release 2.0.0 — Bundle string
-            NSString *v200String = NSLocalizedStringFromTableInBundle(@"v2.0.0_text", @"ReleaseNotes", [NSBundle mainBundle], @"v2.0.0 Release Notes text");
-            notesString = [notesString stringByAppendingString:v200String];
-            
-            // Release 1.0.0 — Bundle string
-            NSString *v100String = NSLocalizedStringFromTableInBundle(@"v1.0.0_text", @"ReleaseNotes", [NSBundle mainBundle], @"v1.0.0 Release Notes text");
-            notesString = [notesString stringByAppendingString:v100String];
-            
+        // Release 2.0.4 — Bundle string
+        NSString *v204String = NSLocalizedStringFromTableInBundle(@"v2.0.4_text", @"ReleaseNotes", [NSBundle mainBundle], @"v2.0.4 Release Notes text");
+        notesString = [notesString stringByAppendingString:v204String];
+        
+        // Release 2.0.3 — Bundle string
+        NSString *v203String = NSLocalizedStringFromTableInBundle(@"v2.0.3_text", @"ReleaseNotes", [NSBundle mainBundle], @"v2.0.3 Release Notes text");
+        notesString = [notesString stringByAppendingString:v203String];
+        
+        // Release 2.0.2 — Bundle string
+        NSString *v202String = NSLocalizedStringFromTableInBundle(@"v2.0.2_text", @"ReleaseNotes", [NSBundle mainBundle], @"v2.0.2 Release Notes text");
+        notesString = [notesString stringByAppendingString:v202String];
+        
+        // Release 2.0.1 — Bundle string
+        NSString *v201String = NSLocalizedStringFromTableInBundle(@"v2.0.1_text", @"ReleaseNotes", [NSBundle mainBundle], @"v2.0.1 Release Notes text");
+        notesString = [notesString stringByAppendingString:v201String];
+        
+        // Release 2.0.0 — Bundle string
+        NSString *v200String = NSLocalizedStringFromTableInBundle(@"v2.0.0_text", @"ReleaseNotes", [NSBundle mainBundle], @"v2.0.0 Release Notes text");
+        notesString = [notesString stringByAppendingString:v200String];
+        
+        // Release 1.0.0 — Bundle string
+        NSString *v100String = NSLocalizedStringFromTableInBundle(@"v1.0.0_text", @"ReleaseNotes", [NSBundle mainBundle], @"v1.0.0 Release Notes text");
+        notesString = [notesString stringByAppendingString:v100String];
+        
         // Attributed strings
         NSMutableAttributedString *notesAttributedString = [[NSMutableAttributedString alloc] initWithString:notesString];
-    
-            // Release 2.0.4 — Attributed string
-            NSRange v204Range = [notesString rangeOfString:@"Version 2.0.4"];
-            NSRange v204DescriptionRange = NSMakeRange(v204Range.location, [@"Version 2.0.4" length]);
-            [notesAttributedString addAttribute:NSFontAttributeName
-                                          value:[UIFont boldSystemFontOfSize:14]
-                                          range:v204DescriptionRange];
-            
-            // Release 2.0.3 — Attributed string
-            NSRange v203Range = [notesString rangeOfString:@"Version 2.0.3 — May 26, 2017"];
-            NSRange v203DescriptionRange = NSMakeRange(v203Range.location, [@"Version 2.0.3 — May 26, 2017" length]);
-            [notesAttributedString addAttribute:NSFontAttributeName
-                                          value:[UIFont boldSystemFontOfSize:14]
-                                          range:v203DescriptionRange];
-
-            // Release 2.0.2 — Attributed string
-            NSRange v202Range = [notesString rangeOfString:@"Version 2.0.2 — May 8, 2017"];
-            NSRange v202DescriptionRange = NSMakeRange(v202Range.location, [@"Version 2.0.2 — May 8, 2017" length]);
-            [notesAttributedString addAttribute:NSFontAttributeName
-                                          value:[UIFont boldSystemFontOfSize:14]
-                                          range:v202DescriptionRange];
-
-            // Release 2.0.1 — Attributed string
-            NSRange v201Range = [notesString rangeOfString:@"Version 2.0.1 — June 26, 2015"];
-            NSRange v201DescriptionRange = NSMakeRange(v201Range.location, [@"Version 2.0.1 — June 26, 2015" length]);
-            [notesAttributedString addAttribute:NSFontAttributeName
-                                          value:[UIFont boldSystemFontOfSize:14]
-                                          range:v201DescriptionRange];
-
-            // Release 2.0.0 — Attributed string
-            NSRange v200Range = [notesString rangeOfString:@"Version 2.0 — April 24, 2015"];
-            NSRange v200DescriptionRange = NSMakeRange(v200Range.location, [@"Version 2.0 — April 24, 2015" length]);
-            [notesAttributedString addAttribute:NSFontAttributeName
-                                          value:[UIFont boldSystemFontOfSize:14]
-                                          range:v200DescriptionRange];
-            
-            // Release 1.0.0 — Attributed string
-            NSRange v100Range = [notesString rangeOfString:@"Version 1.0 — October 17, 2011"];
-            NSRange v100DescriptionRange = NSMakeRange(v100Range.location, [@"Version 1.0 — October 17, 2011" length]);
-            [notesAttributedString addAttribute:NSFontAttributeName
-                                          value:[UIFont boldSystemFontOfSize:14]
-                                          range:v100DescriptionRange];
-            
+        
+        // Release 2.0.4 — Attributed string
+        NSRange v204Range = [notesString rangeOfString:@"Version 2.0.4"];
+        NSRange v204DescriptionRange = NSMakeRange(v204Range.location, [@"Version 2.0.4" length]);
+        [notesAttributedString addAttribute:NSFontAttributeName
+                                      value:[UIFont boldSystemFontOfSize:14]
+                                      range:v204DescriptionRange];
+        
+        // Release 2.0.3 — Attributed string
+        NSRange v203Range = [notesString rangeOfString:@"Version 2.0.3"];
+        NSRange v203DescriptionRange = NSMakeRange(v203Range.location, [@"Version 2.0.3" length]);
+        [notesAttributedString addAttribute:NSFontAttributeName
+                                      value:[UIFont boldSystemFontOfSize:14]
+                                      range:v203DescriptionRange];
+        
+        // Release 2.0.2 — Attributed string
+        NSRange v202Range = [notesString rangeOfString:@"Version 2.0.2"];
+        NSRange v202DescriptionRange = NSMakeRange(v202Range.location, [@"Version 2.0.2" length]);
+        [notesAttributedString addAttribute:NSFontAttributeName
+                                      value:[UIFont boldSystemFontOfSize:14]
+                                      range:v202DescriptionRange];
+        
+        // Release 2.0.1 — Attributed string
+        NSRange v201Range = [notesString rangeOfString:@"Version 2.0.1"];
+        NSRange v201DescriptionRange = NSMakeRange(v201Range.location, [@"Version 2.0.1" length]);
+        [notesAttributedString addAttribute:NSFontAttributeName
+                                      value:[UIFont boldSystemFontOfSize:14]
+                                      range:v201DescriptionRange];
+        
+        // Release 2.0.0 — Attributed string
+        NSRange v200Range = [notesString rangeOfString:@"Version 2.0\n"];
+        NSRange v200DescriptionRange = NSMakeRange(v200Range.location, [@"Version 2.0" length]);
+        [notesAttributedString addAttribute:NSFontAttributeName
+                                      value:[UIFont boldSystemFontOfSize:14]
+                                      range:v200DescriptionRange];
+        
+        // Release 1.0.0 — Attributed string
+        NSRange v100Range = [notesString rangeOfString:@"Version 1.0"];
+        NSRange v100DescriptionRange = NSMakeRange(v100Range.location, [@"Version 1.0" length]);
+        [notesAttributedString addAttribute:NSFontAttributeName
+                                      value:[UIFont boldSystemFontOfSize:14]
+                                      range:v100DescriptionRange];
+        
         self.textView.attributedText = notesAttributedString;
         [self addConstraints];
     }
