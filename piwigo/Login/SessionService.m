2f7f69f6c450548077aceb961543e403bcbc0528
Eddy Lelièvre-Berna <eddy@lelievre-berna.net>
2019-09-30 12:21:39 +0200

    Now remembers that session is closed after network error

--- piwigo/Login/SessionService.m
+++ piwigo/Login/SessionService.m
@@ -103,6 +103,7 @@
                   
                   if(fail)
                   {
+                      [Model sharedInstance].hadOpenedSession = NO;
                       fail(task, error);
                   }
               }];
