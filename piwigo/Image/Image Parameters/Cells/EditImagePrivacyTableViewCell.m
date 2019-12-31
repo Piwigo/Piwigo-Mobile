8a943938fc131ed8102ca9a6b68f6db666c9d5eb
Eddy Lelièvre-Berna <eddy@lelievre-berna.net>
2019-12-31 17:19:52 +0100

    Renamed IBOutlets

---
+++ piwigo/Image/Image Parameters/Cells/EditImagePrivacyTableViewCell.m
@@ -0,0 +1,49 @@
+//
+//  EditImagePrivacyTableViewCell.m
+//  piwigo
+//
+//  Created by Spencer Baker on 2/16/15.
+//  Copyright (c) 2015 bakercrew. All rights reserved.
+//
+
+#import "EditImagePrivacyTableViewCell.h"
+#import "Model.h"
+
+@interface EditImagePrivacyTableViewCell()
+
+@property (weak, nonatomic) IBOutlet UILabel *label;
+@property (weak, nonatomic) IBOutlet UILabel *list;
+
+@end
+
+@implementation EditImagePrivacyTableViewCell
+
+- (void)awakeFromNib {
+
+    // Initialization code
+    [super awakeFromNib];
+	
+    self.label.font = [UIFont piwigoFontNormal];
+    self.list.font = [UIFont piwigoFontNormal];
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+    // Configure the view for the selected state
+	
+}
+
+-(void)setLeftLabelText:(NSString*)text
+{
+    self.label.text = text;
+    self.label.textColor = [UIColor piwigoRightLabelColor];
+}
+
+-(void)setPrivacyLevel:(NSInteger)privacy
+{
+	self.list.text = [[Model sharedInstance] getNameForPrivacyLevel:privacy];
+    self.list.textColor = [UIColor piwigoLeftLabelColor];
+}
+
+@end
