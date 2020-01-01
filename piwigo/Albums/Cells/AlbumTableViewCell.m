b367635d864070e81ae028646582b4132eb8e06b
Eddy Lelièvre-Berna <eddy@lelievre-berna.net>
2020-01-01 13:48:59 +0100

    Fixes inappropriate background colour revealed when using swipe cells

--- piwigo/Albums/Cells/AlbumTableViewCell.m
+++ piwigo/Albums/Cells/AlbumTableViewCell.m
@@ -111,6 +111,7 @@ NSString * const kAlbumTableCell_ID = @"AlbumTableViewCell";
         self.handleButton.hidden = NO;
 
         // Right => Left swipe
+        self.swipeBackgroundColor = [UIColor piwigoOrange];
         self.rightSwipeSettings.transition = MGSwipeTransitionBorder;
         self.rightButtons = @[[MGSwipeButton buttonWithTitle:@""
                                                         icon:[UIImage imageNamed:@"swipeTrash.png"]
