edda8aac71b39eb4f010cce8c43d48a81fa2d8f2
Eddy Lelièvre-Berna <eddy@lelievre-berna.net>
2020-02-15 19:35:58 +0100

    Fixes #297 — Step #3: adds Core Data Tag entity management in Swift

---
+++ piwigo/Data Cache/Tags Data/Tag.swift
@@ -0,0 +1,177 @@
+//
+//  Tag.swift
+//  piwigo
+//
+//  Created by Eddy Lelièvre-Berna on 19/01/2020.
+//  Copyright © 2020 Piwigo.org. All rights reserved.
+//
+//  An NSManagedObject subclass for the Tag entity.
+
+import CoreData
+
+// MARK: - Core Data
+/**
+ Managed object subclass for the Tag entity.
+ */
+
+@objc
+class Tag: NSManagedObject {
+
+    // A unique identifier for removing duplicates. Constrain
+    // the PiwigoTagData entity on this attribute in the data model editor.
+    @NSManaged var tagId: NSInteger
+    
+    // The other attributes of a tag.
+    @NSManaged var tagName: String
+    @NSManaged var lastModified: Date
+    @NSManaged var numberOfImagesUnderTag : NSInteger
+
+    // Singleton
+    @objc static let sharedInstance: Tag = Tag()
+    
+    /**
+     Updates a Tag instance with the values from a TagProperties.
+     */
+    func update(with tagProperties: TagProperties) throws {
+        
+        // Update the tag only if the Id and Name properties have values.
+        guard let newId = tagProperties.id,
+              let newName = tagProperties.name else {
+                throw TagError.missingData
+        }
+        tagId = newId
+        tagName = newName
+
+        // In the absence of date, use today
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+        lastModified = dateFormatter.date(from: tagProperties.lastmodified ?? "") ?? Date()
+
+        // In the absence of count, use max integer
+        if let newCount = tagProperties.counter {
+            numberOfImagesUnderTag = newCount
+        } else {
+            numberOfImagesUnderTag = Int.max
+        }
+    }
+}
+
+// MARK: - Codable, kPiwigoTagsGetList
+/**
+ A struct for decoding JSON with the following structure returned by kPiwigoTagsGetList:
+
+{"result":
+   {"tags":[
+       {"id":1,"lastmodified":"2018-08-23 15:30:43","counter":11,"name":"Birthday","url":"https:…","url_name":"birthday"},
+       {"id":14,"lastmodified":"2019-08-27 20:33:01","counter":5,"name":"Cities","url":"https:…","url_name":"cities"},
+       {"id":2,"lastmodified":"2018-08-23 15:30:43","counter":9,"name":"Piwigo","url":"https:…","url_name":"piwigo"}
+       ]
+   },
+"stat":"ok"}
+ 
+ or with the following structure returned by kPiwigoTagsGetAdminList:
+
+ {"result":
+    {"tags":[
+        {"id":"1","lastmodified":"2018-08-23 15:30:43","name":"Birthday","url_name":"birthday"},
+        {"id":"14","lastmodified":"2019-08-27 20:33:01","name":"Cities","url_name":"cities"},
+        {"id":"2","lastmodified":"2018-08-23 15:30:43","name":"Piwigo","url_name":"piwigo"},
+        {"id":"18","lastmodified":"2020-02-08 18:02:22","name":"Test Tag","url_name":"test_tag"}
+        ]
+    },
+ "stat":"ok"}
+
+ Stores an array of decoded TagProperties for later use in creating or updating Tag instances.
+*/
+struct TagJSON: Decodable {
+
+    private enum RootCodingKeys: String, CodingKey {
+        case stat
+        case result
+        case err
+        case message
+    }
+
+    private enum ResultCodingKeys: String, CodingKey {
+        case tags
+        case id
+        case name
+        case lastmodified
+        case url_name
+    }
+
+    // Constants
+    var stat: String?
+    var errorCode = 0
+    var errorMessage = ""
+    
+    // A TagProperties array of decoded Tag data.
+    var tagPropertiesArray = [TagProperties]()
+
+    init(from decoder: Decoder) throws
+    {
+        // Root container keyed by RootCodingKeys
+        let rootContainer = try decoder.container(keyedBy: RootCodingKeys.self)
+        
+        // Status returned by Piwigo
+        stat = try rootContainer.decodeIfPresent(String.self, forKey: .stat)
+        if (stat == "ok")
+        {
+            // Result container keyed by ResultCodingKeys
+            let resultContainer = try rootContainer.nestedContainer(keyedBy: ResultCodingKeys.self, forKey: .result)
+//            dump(resultContainer)
+            
+            // Decodes tags from the data and store them in the array
+            do {
+                // Use TagProperties struct
+                try tagPropertiesArray = resultContainer.decode([TagProperties].self, forKey: .tags)
+            }
+            catch {
+                // Use a different struct because id is a String instead of an Int
+                let tagPropertiesArray4Admin = try resultContainer.decode([TagProperties4Admin].self, forKey: .tags)
+                
+                // Inject data into TagProperties after converting id
+                for tagProperty4Admin in tagPropertiesArray4Admin {
+                    let id:Int? = Int(tagProperty4Admin.id ?? "")!
+                    let tagProperty = TagProperties(id: id, name: tagProperty4Admin.name, lastmodified: tagProperty4Admin.lastmodified, url_name: tagProperty4Admin.url_name, counter: Int.max, url: "")
+                    tagPropertiesArray.append(tagProperty)
+                }
+            }
+        }
+        else if (stat == "fail")
+        {
+            // Retrieve Piwigo server error
+            errorCode = try rootContainer.decode(Int.self, forKey: .err)
+            errorMessage = try rootContainer.decode(String.self, forKey: .message)
+        }
+        else {
+            // Unexpected Piwigo server error
+        }
+    }
+}
+
+/**
+ A struct for decoding JSON returned by kPiwigoTagsGetList.
+ All members are optional in case they are missing from the data.
+*/
+struct TagProperties: Codable
+{
+    let id: Int?                    // 1
+    let name: String?               // "Birthday"
+    let lastmodified: String?       // "2018-08-23 15:30:43"
+    let url_name: String?           // "birthday"
+    let counter: Int?               // 8
+    let url: String?                // "https:…"
+}
+
+/**
+ A struct for decoding JSON returned by kPiwigoTagsGetAdminList:
+ All members are optional in case they are missing from the data.
+*/
+struct TagProperties4Admin: Codable
+{
+    let id: String?                 // 1 (String instead of Int)
+    let name: String?               // "Birthday"
+    let lastmodified: String?       // "2018-08-23 15:30:43"
+    let url_name: String?           // "birthday"
+}
---
+++ piwigo/Data Cache/Tags Data/TagsProvider.swift
@@ -0,0 +1,257 @@
+//
+//  TagsProvider.swift
+//  piwigo
+//
+//  Created by Eddy Lelièvre-Berna on 19/01/2020.
+//  Copyright © 2020 Piwigo.org. All rights reserved.
+//
+//  A class to fetch data from the remote server and save it to the Core Data store.
+
+import CoreData
+
+class TagsProvider {
+
+    // MARK: - Piwigo API methods
+
+    let kPiwigoTagsGetList = "format=json&method=pwg.tags.getList"
+    let kPiwigoTagsGetAdminList = "format=json&method=pwg.tags.getAdminList"
+    let kPiwigoTagsGetImages = "format=json&method=pwg.tags.getImages"
+    let kPiwigoTagsAdd = "format=json&method=pwg.tags.add"
+    
+    
+    // MARK: - Core Data methods
+    
+    /**
+     The persistent container to set up the Core Data stack.
+     */
+    var managedObjectContext: NSManagedObjectContext
+
+    init(completionClosure: @escaping () -> ())
+    {
+        if #available(iOS 10.0, *) {
+            managedObjectContext = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        } else {
+            // iOS 9.x and below
+            managedObjectContext = (UIApplication.shared.delegate as! AppDelegate).managedObjectContext
+        }
+    }
+
+    // MARK: Fetch Tags
+    /**
+     Fetches the tag feed from the remote Piwigo server, and imports it into Core Data.
+    */
+    func fetchTags(asAdmin: Bool, completionHandler: @escaping (Error?) -> Void) {
+
+        NetworkHandler.post(asAdmin ? kPiwigoTagsGetAdminList : kPiwigoTagsGetList, urlParameters: nil, parameters: nil, progress: nil, success: { (task, jsonData) in
+
+            // Alert the user if no data comes back.
+            guard let data = try? JSONSerialization.data(withJSONObject:jsonData ?? "") else {
+                completionHandler(TagError.networkUnavailable)
+                return
+            }
+
+            // Decode the JSON and import it into Core Data.
+            do {
+                // Decode the JSON into codable type TagJSON.
+                let decoder = JSONDecoder()
+                let tagJSON = try decoder.decode(TagJSON.self, from: data)
+
+                // Piwigo error?
+                let error: NSError
+                if (tagJSON.errorCode != 0) {
+                    error = NSError.init(domain: "Piwigo", code: tagJSON.errorCode, userInfo: [NSLocalizedDescriptionKey : tagJSON.errorMessage])
+                    completionHandler(error)
+                    return
+                }
+
+                // Import the tagJSON into Core Data.
+                try self.importTags(from: tagJSON)
+
+            } catch {
+                // Alert the user if data cannot be digested.
+                completionHandler(TagError.wrongDataFormat)
+                return
+            }
+            completionHandler(nil)
+        }) { (URLSessionTask, Error) in
+
+            completionHandler(TagError.networkUnavailable)
+        }
+    }
+    
+    /**
+     Imports a JSON dictionary into the Core Data store on a private queue,
+     processing the record in batches to avoid a high memory footprint.
+    */
+    private func importTags(from tagJSON: TagJSON) throws {
+        
+        guard !tagJSON.tagPropertiesArray.isEmpty else { return }
+        
+        // Create a private queue context.
+        let taskContext: NSManagedObjectContext
+        if #available(iOS 10.0, *) {
+            taskContext = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.newBackgroundContext()
+        } else {
+            // iOS 9.0 and below - however you were previously handling it
+            guard let modelURL = Bundle.main.url(forResource: "DataModel", withExtension:"momd") else {
+                fatalError("Error loading model from bundle")
+            }
+            guard let mom = NSManagedObjectModel(contentsOf: modelURL) else {
+                fatalError("Error initializing mom from: \(modelURL)")
+            }
+            let psc = NSPersistentStoreCoordinator(managedObjectModel: mom)
+            taskContext = NSManagedObjectContext(concurrencyType: .privateQueueConcurrencyType)
+            taskContext.parent = managedObjectContext;
+            let urls = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+            let docURL = urls[urls.endIndex-1]
+            let storeURL = docURL.appendingPathComponent("DataModel.sqlite")
+            do {
+                try psc.addPersistentStore(ofType: NSSQLiteStoreType, configurationName: nil, at: storeURL, options: nil)
+            } catch {
+                fatalError("Error migrating store: \(error)")
+            }
+        }
+        taskContext.mergePolicy = NSMergeByPropertyObjectTrumpMergePolicy
+        // Set unused undoManager to nil for macOS (it is nil by default on iOS)
+        // to reduce resource requirements.
+        taskContext.undoManager = nil
+                
+        // Process records in batches to avoid a high memory footprint.
+        let batchSize = 256
+        let count = tagJSON.tagPropertiesArray.count
+        
+        // Determine the total number of batches.
+        var numBatches = count / batchSize
+        numBatches += count % batchSize > 0 ? 1 : 0
+        
+        for batchNumber in 0 ..< numBatches {
+            
+            // Determine the range for this batch.
+            let batchStart = batchNumber * batchSize
+            let batchEnd = batchStart + min(batchSize, count - batchNumber * batchSize)
+            let range = batchStart..<batchEnd
+            
+            // Create a batch for this range from the decoded JSON.
+            let tagsBatch = Array(tagJSON.tagPropertiesArray[range])
+            
+            // Stop the entire import if any batch is unsuccessful.
+            if !importOneBatch(tagsBatch, taskContext: taskContext) {
+                return
+            }
+        }
+    }
+    
+    /**
+     Imports one batch of tags, creating managed objects from the new data,
+     and saving them to the persistent store, on a private queue. After saving,
+     resets the context to clean up the cache and lower the memory footprint.
+     
+     NSManagedObjectContext.performAndWait doesn't rethrow so this function
+     catches throws within the closure and uses a return value to indicate
+     whether the import is successful.
+    */
+    private func importOneBatch(_ tagsBatch: [TagProperties], taskContext: NSManagedObjectContext) -> Bool {
+        
+        var success = false
+
+        // taskContext.performAndWait runs on the URLSession's delegate queue
+        // so it won’t block the main thread.
+        taskContext.performAndWait {
+            
+            // Create a new record for each tag in the batch.
+            for tagData in tagsBatch {
+                
+                // Create a Tag managed object on the private queue context.
+                guard let tag = NSEntityDescription.insertNewObject(forEntityName: "Tag", into: taskContext) as? Tag else {
+                    print(TagError.creationError.localizedDescription)
+                    return
+                }
+                
+                // Populate the Tag's properties using the raw data.
+                do {
+                    try tag.update(with: tagData)
+                }
+                catch TagError.missingData {
+                    // Delete invalid Tag from the private queue context.
+                    print(TagError.missingData.localizedDescription)
+                    taskContext.delete(tag)
+                }
+                catch {
+                    print(error.localizedDescription)
+                }
+            }
+            
+            // Save all insertions and deletions from the context to the store.
+            if taskContext.hasChanges {
+                do {
+                    try taskContext.save()
+                }
+                catch {
+                    print("Error: \(error)\nCould not save Core Data context.")
+                    return
+                }
+                // Reset the taskContext to free the cache and lower the memory footprint.
+                taskContext.reset()
+            }
+
+            success = true
+        }
+        return success
+    }
+    
+    
+    // MARK: Clear Tags
+    /**
+     Clear cached Core Data tag entries
+    */
+    func clearTags() {
+        
+        // Create a fetch request for the Tag entity
+        let fetchRequest = NSFetchRequest<Tag>(entityName: "Tag")
+
+        // Create batch delete request
+        let batchDeleteRequest = NSBatchDeleteRequest(fetchRequest: fetchRequest as! NSFetchRequest<NSFetchRequestResult>)
+
+        // Execute batch delete request
+        do {
+            try managedObjectContext.execute(batchDeleteRequest)
+        }
+        catch {
+            fatalError("Unresolved error \(error)")
+        }
+    }
+
+
+    // MARK: - NSFetchedResultsController
+    
+    /**
+     A fetched results controller delegate to give consumers a chance to update
+     the user interface when content changes.
+     */
+    weak var fetchedResultsControllerDelegate: NSFetchedResultsControllerDelegate?
+    
+    /**
+     A fetched results controller to fetch Tag records sorted by name.
+     */
+    lazy var fetchedResultsController: NSFetchedResultsController<Tag> = {
+        
+        // Create a fetch request for the Tag entity sorted by name.
+        let fetchRequest = NSFetchRequest<Tag>(entityName: "Tag")
+        fetchRequest.sortDescriptors = [NSSortDescriptor(key: "tagName", ascending: true)]
+        
+        // Create a fetched results controller and set its fetch request, context, and delegate.
+        let controller = NSFetchedResultsController(fetchRequest: fetchRequest,
+                                            managedObjectContext: self.managedObjectContext,
+                                              sectionNameKeyPath: nil, cacheName: nil)
+        controller.delegate = fetchedResultsControllerDelegate
+        
+        // Perform the fetch.
+        do {
+            try controller.performFetch()
+        } catch {
+            fatalError("Unresolved error \(error)")
+        }
+        
+        return controller
+    }()
+}
