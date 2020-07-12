2e90c36499a17bcd33ef617500d19b9e375450fb
Eddy Lelièvre-Berna <eddy@lelievre-berna.net>
2020-07-12 18:31:48 +0200

    Removes useless code

--- piwigo/Albums/AlbumImagesViewController.m
+++ piwigo/Albums/AlbumImagesViewController.m
@@ -2302,15 +2302,6 @@ NSString * const kPiwigoNotificationDeletedImage = @"kPiwigoNotificationDeletedI
     [ImageService deleteImages:self.selectedImagesToDelete
               ListOnCompletion:^(NSURLSessionTask *task) {
                   
-                  // Images deleted
-//                  for (PiwigoImageData *selectedImage in self.selectedImagesToDelete) {
-//                      [self.albumData removeImageWithId:selectedImage.imageId];
-//                      [self.selectedImageIds removeObject:[NSString stringWithFormat:@"%ld", (long)selectedImage.imageId]];
-//                  }
-                  
-                  // Reload collection
-//                  [self.imagesCollection reloadData];
-
                   // Hide HUD
                   dispatch_async(dispatch_get_main_queue(), ^{
                       [self hideHUDwithSuccess:YES completion:^{
