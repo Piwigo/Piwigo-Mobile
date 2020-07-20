355f90e5d2b2c8ace73558e8b78187ca86ed6f17
Eddy Lelièvre-Berna <eddy@lelievre-berna.net>
2020-07-20 18:19:35 +0200

    Provides only tag IDs to setImageInfoWithId()

--- piwigo/Upload/Upload Services/UploadVideo.swift
+++ piwigo/Upload/Upload Services/UploadVideo.swift
@@ -176,7 +176,8 @@ class UploadVideo {
                 requestDelete: upload.requestDelete, requestError: error.localizedDescription,
                 creationDate: upload.creationDate, fileName: upload.fileName, mimeType: upload.mimeType,
                 isVideo: upload.isVideo, author: upload.author, privacyLevel: upload.privacyLevel,
-                imageTitle: upload.imageTitle, comment: upload.comment, tags: upload.tags, imageId: upload.imageId)
+                imageTitle: upload.imageTitle, comment: upload.comment, tagIds: upload.tagIds,
+                imageId: upload.imageId)
             
             // Update request with error description
             print("    >", error.localizedDescription)
@@ -193,7 +194,8 @@ class UploadVideo {
             requestDelete: upload.requestDelete, requestError: "",
             creationDate: upload.creationDate, fileName: upload.fileName, mimeType: upload.mimeType,
             isVideo: upload.isVideo, author: upload.author, privacyLevel: upload.privacyLevel,
-            imageTitle: upload.imageTitle, comment: upload.comment, tags: upload.tags, imageId: upload.imageId)
+            imageTitle: upload.imageTitle, comment: upload.comment, tagIds: upload.tagIds,
+            imageId: upload.imageId)
 
         // Update request ready for transfer
         print("    > prepared file \(uploadProperties.fileName!)")
