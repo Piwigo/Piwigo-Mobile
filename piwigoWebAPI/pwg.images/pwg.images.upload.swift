3e0ff0c217468b11f2b0f633890a6fa90ce49edc
Eddy Lelièvre-Berna <eddy@lelievre-berna.net>
2020-07-25 00:33:05 +0200

    Completes JSON decoding

--- piwigoWebAPI/pwg.images/pwg.images.upload.swift
+++ piwigoWebAPI/pwg.images/pwg.images.upload.swift
@@ -13,62 +13,73 @@ let kPiwigoImagesUpload = "format=json&method=pwg.images.upload"
 
 struct ImagesUploadJSON: Decodable {
 
-    private enum RootCodingKeys: String, CodingKey {
-        case stat
-        case result
-        case err
-        case message
-    }
-
-    // Constants
-    var stat: String?
+    var status: String?
+    var data = ImagesUpload.init(image_id: NSNotFound, category: nil, name: "", square_src: "", src: "")
     var errorCode = 0
     var errorMessage = ""
-    
-    // An UploadProperties array of decoded ImagesUpload data.
-    var imagesUpload = ImagesUpload.init(image_id: NSNotFound, name: "", square_src: "", src: "")
+
+    private enum RootCodingKeys: String, CodingKey {
+        case status = "stat"
+        case data = "result"
+        case err = "err"
+        case message = "message"
+    }
 
     init(from decoder: Decoder) throws
     {
-        // Root container keyed by RootCodingKeys
-        guard let rootContainer = try? decoder.container(keyedBy: RootCodingKeys.self) else {
-            return
-        }
-//        dump(rootContainer)
+        do {
+            // Root container keyed by RootCodingKeys
+            guard let rootContainer = try? decoder.container(keyedBy: RootCodingKeys.self) else {
+                return
+            }
+    //        dump(rootContainer)
 
-        // Status returned by Piwigo
-        stat = try rootContainer.decodeIfPresent(String.self, forKey: .stat)
-        if (stat == "ok")
-        {
-            // Decodes response from the data and store them in the array
-            imagesUpload = try rootContainer.decode(ImagesUpload.self, forKey: .result)
-//            dump(imagesUpload)
-        }
-        else if (stat == "fail")
-        {
-            // Retrieve Piwigo server error
-            errorCode = try rootContainer.decode(Int.self, forKey: .err)
-            errorMessage = try rootContainer.decode(String.self, forKey: .message)
-        }
-        else {
-            // Unexpected Piwigo server error
-            errorCode = -1
-            errorMessage = "Unexpected error encountered while calling server method with provided parameters."
+            // Status returned by Piwigo
+            status = try rootContainer.decodeIfPresent(String.self, forKey: .status)
+            if (status == "ok")
+            {
+                // Decodes response from the data and store them in the array
+                data = try rootContainer.decode(ImagesUpload.self, forKey: .data)
+    //            dump(imagesUpload)
+            }
+            else if (status == "fail")
+            {
+                // Retrieve Piwigo server error
+                errorCode = try rootContainer.decode(Int.self, forKey: .err)
+                errorMessage = try rootContainer.decode(String.self, forKey: .message)
+            }
+            else {
+                // Unexpected Piwigo server error
+                errorCode = -1
+                errorMessage = "Unexpected error encountered while calling server method with provided parameters."
+            }
+        } catch let error {
+            print(error.localizedDescription)
         }
     }
 }
 
-/**
- A struct for decoding JSON returned by kPiwigoImagesUpload.
- All members are optional in case they are missing from the data.
-*/
+// MARK: - Result
 struct ImagesUpload: Codable
 {
     let image_id: Int?              // 1042
 
     // The following data are not used yet
-//    let category: [String]          // {"id":140,"nb_photos":"7","label":"Essai"}
+    let category: Category?         // {"id":140,"nb_photos":"7","label":"Essai"}
     let name: String?               // "Delft - 01"
     let square_src: String?         // "https://…-sq.jpg"
     let src: String?                // "https://…-th.jpg"
+}
+
+// MARK: - Category
+struct Category: Codable {
+    let catId: Int?
+    let catName: String?
+    let nbPhotos: String?
+
+    enum CodingKeys: String, CodingKey {
+        case catId = "id"
+        case catName = "label"
+        case nbPhotos = "nb_photos"
+    }
 }
