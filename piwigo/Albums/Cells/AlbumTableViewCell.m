680db6e0524a51eedec39cc688ea0b35da299fc9
Eddy Lelièvre-Berna <eddy@lelievre-berna.net>
2020-08-26 21:10:32 +0200

    Set swipe button background colour to orange so that the Edit button looks infinite

--- piwigo/Albums/Cells/AlbumTableViewCell.m
+++ piwigo/Albums/Cells/AlbumTableViewCell.m
@@ -110,6 +110,7 @@ NSString * const kAlbumTableCell_ID = @"AlbumTableViewCell";
         self.handleButton.hidden = NO;
 
         // Right => Left swipe
+        self.swipeBackgroundColor = [UIColor piwigoColorOrange];
         self.rightSwipeSettings.transition = MGSwipeTransitionBorder;
         self.rightButtons = @[[MGSwipeButton buttonWithTitle:@""
                                                         icon:[UIImage imageNamed:@"swipeTrash.png"]
