21f978836f08a17b89c366aba5a5ca1a19094963
Eddy Lelièvre-Berna <eddy@lelievre-berna.net>
2020-09-06 12:35:12 +0200

    Fixes #391

--- piwigo/Login/LoginViewController.m
+++ piwigo/Login/LoginViewController.m
@@ -1053,15 +1053,19 @@ NSString * const kPiwigoSupport = @"— iOS@piwigo.org —";
 
 -(BOOL)textFieldShouldBeginEditing:(UITextField *)textField
 {
-    // Disable HTTP login action until user provides infos
+    // Disable HTTP login action until user provides credentials
     if (self.httpAlertController != nil) {
-        if (textField == [self.httpAlertController.textFields objectAtIndex:0]) {
-            if ([self.httpAlertController.textFields objectAtIndex:0].text.length == 0)
-                [self.httpLoginAction setEnabled:NO];
-        }
-        else if (textField == [self.httpAlertController.textFields objectAtIndex:1]) {
-            if ([self.httpAlertController.textFields objectAtIndex:1].text.length == 0)
-                [self.httpLoginAction setEnabled:NO];
+        // Requesting autorisation to access non secure web site
+        // or asking HTTP basic authentication credentials
+        if (self.httpAlertController.textFields.count > 0) {
+            // Being requesting HTTP basic authentication credentials
+            if (textField == [self.httpAlertController.textFields objectAtIndex:0]) {
+                if ([self.httpAlertController.textFields objectAtIndex:0].text.length == 0)
+                    [self.httpLoginAction setEnabled:NO];
+            } else if (textField == [self.httpAlertController.textFields objectAtIndex:1]) {
+                if ([self.httpAlertController.textFields objectAtIndex:1].text.length == 0)
+                    [self.httpLoginAction setEnabled:NO];
+            }
         }
     }
     return YES;
@@ -1074,9 +1078,14 @@ NSString * const kPiwigoSupport = @"— iOS@piwigo.org —";
         [self.loginButton setEnabled:NO];
     }
     else if (self.httpAlertController != nil) {
-        if ((textField == [self.httpAlertController.textFields objectAtIndex:0]) ||
-            (textField == [self.httpAlertController.textFields objectAtIndex:1])) {
-            [self.httpLoginAction setEnabled:NO];
+        // Requesting autorisation to access non secure web site
+        // or asking HTTP basic authentication credentials
+        if (self.httpAlertController.textFields.count > 0) {
+            // Being requesting HTTP basic authentication credentials
+            if ((textField == [self.httpAlertController.textFields objectAtIndex:0]) ||
+                (textField == [self.httpAlertController.textFields objectAtIndex:1])) {
+                [self.httpLoginAction setEnabled:NO];
+            }
         }
     }
     
@@ -1087,17 +1096,22 @@ NSString * const kPiwigoSupport = @"— iOS@piwigo.org —";
 {
     NSString *finalString = [textField.text stringByReplacingCharactersInRange:range withString:string];
     
-    if (self.httpAlertController != nil) {
-        if ((textField == [self.httpAlertController.textFields objectAtIndex:0]) ||
-            (textField == [self.httpAlertController.textFields objectAtIndex:1])) {
-            // Enable HTTP Login action if field not empty
-            [self.httpLoginAction setEnabled:(finalString.length >= 1)];
-        }
-    }
-    else if (textField == self.serverTextField) {
+    if (textField == self.serverTextField) {
         // Disable Login button if URL invalid
         [self saveServerAddress:finalString andUsername:self.userTextField.text];
         [self.loginButton setEnabled:YES];
+    }
+    else if (self.httpAlertController != nil) {
+        // Requesting autorisation to access non secure web site
+        // or asking HTTP basic authentication credentials
+        if (self.httpAlertController.textFields.count > 0) {
+            // Being requesting HTTP basic authentication credentials
+            if ((textField == [self.httpAlertController.textFields objectAtIndex:0]) ||
+                (textField == [self.httpAlertController.textFields objectAtIndex:1])) {
+                // Enable HTTP Login action if field not empty
+                [self.httpLoginAction setEnabled:(finalString.length >= 1)];
+            }
+        }
     }
     
     return YES;
@@ -1105,7 +1119,7 @@ NSString * const kPiwigoSupport = @"— iOS@piwigo.org —";
 
 -(BOOL)textFieldShouldReturn:(UITextField *)textField
 {
-	if(textField == self.serverTextField) {
+	if (textField == self.serverTextField) {
         // Save server address and username to disk
         BOOL validURL = [self saveServerAddress:self.serverTextField.text andUsername:self.userTextField.text];
         [self.loginButton setEnabled:validURL];
