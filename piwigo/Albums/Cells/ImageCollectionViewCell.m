e2c607169f2e4d072c5def7a9f727f9e62133d9b
Eddy Lelièvre-Berna <eddy@lelievre-berna.net>
2020-09-19 17:04:50 +0200

    Adds missing [super prepareForReuse]

--- piwigo/Albums/Cells/ImageCollectionViewCell.m
+++ piwigo/Albums/Cells/ImageCollectionViewCell.m
@@ -305,7 +305,9 @@
 
 -(void)prepareForReuse
 {
-	self.cellImage.image = nil;
+    [super prepareForReuse];
+    
+    self.cellImage.image = nil;
 	self.isSelected = NO;
 	self.playImage.hidden = YES;
 	self.noDataLabel.hidden = YES;
