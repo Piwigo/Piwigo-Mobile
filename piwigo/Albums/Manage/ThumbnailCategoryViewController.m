c6eac81ccd3dcf7c628cebe74d27b6ada0e80945
Eddy Lelièvre-Berna <eddy@lelievre-berna.net>
2020-11-10 12:52:50 +0100

    Adopt InsetGrouped style

--- piwigo/Albums/Manage/ThumbnailCategoryViewController.m
+++ piwigo/Albums/Manage/ThumbnailCategoryViewController.m
@@ -46,7 +46,11 @@ CGFloat const kAllCategoriesWidth = 512.0;      // AllCategories view width
         self.categoriesThatShowSubCategories = [NSMutableArray new];
 
         // Table view
-        self.categoriesTableView = [[UITableView alloc] initWithFrame:CGRectZero style:UITableViewStyleGrouped];
+        if (@available(iOS 13.0, *)) {
+            self.categoriesTableView = [[UITableView alloc] initWithFrame:CGRectZero style:UITableViewStyleInsetGrouped];
+        } else {
+            self.categoriesTableView = [[UITableView alloc] initWithFrame:CGRectZero style:UITableViewStyleGrouped];
+        }
         self.categoriesTableView.translatesAutoresizingMaskIntoConstraints = NO;
         self.categoriesTableView.backgroundColor = [UIColor clearColor];
         self.categoriesTableView.alwaysBounceVertical = YES;
