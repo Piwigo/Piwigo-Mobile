297baff13029c1908a3e47eb92dee4358c09aaab
Eddy Lelièvre-Berna <eddy@lelievre-berna.net>
2020-12-23 15:24:58 +0100

    Add missing parameters to updateCell()

--- piwigo/Upload/Upload Services/UploadVideo.swift
+++ piwigo/Upload/Upload Services/UploadVideo.swift
@@ -198,7 +198,8 @@ extension UploadManager {
         }
 
         // Update UI
-        updateCell(with: newProperties.localIdentifier, stateLabel: newProperties.stateLabel)
+        updateCell(with: newProperties.localIdentifier, stateLabel: newProperties.stateLabel,
+                   photoResize: nil, progress: Float(0.0), errorMsg: errorMsg)
 
         // Update state of upload request
         print("\(debugFormatter.string(from: Date())) > prepared \(uploadID) \(errorMsg)")
