4b26e8ba052e7aa4b218c64c526f23f933682094
Eddy Lelièvre-Berna <eddy@lelievre-berna.net>
2021-01-02 19:35:48 +0100

    Fixes #384 — Adds "Select All" button to clipboard section

--- piwigo/Upload/Pick Pasteboard Images/PasteboardImagesHeaderReusableView.swift
+++ piwigo/Upload/Pick Pasteboard Images/PasteboardImagesHeaderReusableView.swift
@@ -8,15 +8,21 @@
 
 import UIKit
 
+@objc protocol PasteboardImagesHeaderDelegate: NSObjectProtocol {
+    func didSelectImagesOfSection()
+}
+
 @objc
 class PasteboardImagesHeaderReusableView: UICollectionReusableView {
     
     // MARK: - View
-    
-    @IBOutlet weak var headerLabel: UILabel!
 
-    @objc
-    func configure() {
+    @objc weak var headerDelegate: PasteboardImagesHeaderDelegate?
+
+    @IBOutlet weak var headerLabel: UILabel!
+    @IBOutlet weak var selectButton: UIButton!
+    
+    func configure(with selectState: SelectButtonState) {
         
         // General settings
         backgroundColor = UIColor.piwigoColorBackground().withAlphaComponent(0.75)
@@ -24,11 +30,43 @@ class PasteboardImagesHeaderReusableView: UICollectionReusableView {
         // Data label used when place name known
         headerLabel.textColor = UIColor.piwigoColorHeader()
         headerLabel.text = NSLocalizedString("imageUploadPasteboard", comment: "Clipboard")
+
+        // Select/deselect button
+        selectButton.layer.cornerRadius = 13.0
+        setButtonTitle(for: selectState)
+    }
+
+    @IBAction func tappedSelectButton(_ sender: Any) {
+        // Select/deselect images
+        headerDelegate?.didSelectImagesOfSection()
     }
 
     override func prepareForReuse() {
         super.prepareForReuse()
 
         headerLabel.text = ""
+        selectButton.setTitle("", for: .normal)
+        selectButton.backgroundColor = UIColor.piwigoColorBackground()
+    }
+
+
+    // MARK: Utilities
+    
+    private func setButtonTitle(for state:SelectButtonState) {
+        let title: String, bckgColor: UIColor
+        switch state {
+        case .select:
+            title = String(format: "  %@  ", NSLocalizedString("selectAll", comment: "Select All"))
+            bckgColor = UIColor.piwigoColorCellBackground()
+        case .deselect:
+            title = String(format: "  %@  ", NSLocalizedString("categoryImageList_deselectButton", comment: "Deselect"))
+            bckgColor = UIColor.piwigoColorCellBackground()
+        case .none:
+            title = ""
+            bckgColor = UIColor.piwigoColorBackground()
+        }
+        selectButton.backgroundColor = bckgColor
+        selectButton.setTitle(title, for: .normal)
+        selectButton.setTitleColor(UIColor.piwigoColorWhiteCream(), for: .normal)
     }
 }
