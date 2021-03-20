228e28d2899631ca69cefb1194dfcf43f350f83e
Eddy Lelièvre-Berna <eddy@lelievre-berna.net>
2021-03-20 13:39:02 +0100

    Adds option for enabling/disabling auto-uploads (see #70)

--- piwigo/Settings/Upload/AutoUploadViewController.swift
+++ piwigo/Settings/Upload/AutoUploadViewController.swift
@@ -9,8 +9,221 @@
 import UIKit
 
 @objc
-class AutoUploadViewController: UIViewController {
+class AutoUploadViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     @IBOutlet var autoUploadTableView: UITableView!
     
+    
+    // MARK: - View Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        title = NSLocalizedString("tabBar_upload", comment: "Upload")
+    }
+
+    @objc func applyColorPalette() {
+        // Background color of the view
+        view.backgroundColor = UIColor.piwigoColorBackground()
+
+        // Navigation bar
+        let attributes = [
+            NSAttributedString.Key.foregroundColor: UIColor.piwigoColorWhiteCream(),
+            NSAttributedString.Key.font: UIFont.piwigoFontNormal()
+        ]
+        navigationController?.navigationBar.titleTextAttributes = attributes as [NSAttributedString.Key : Any]
+        if #available(iOS 11.0, *) {
+            navigationController?.navigationBar.prefersLargeTitles = false
+        }
+        navigationController?.navigationBar.barStyle = Model.sharedInstance().isDarkPaletteActive ? .black : .default
+        navigationController?.navigationBar.tintColor = UIColor.piwigoColorOrange()
+        navigationController?.navigationBar.barTintColor = UIColor.piwigoColorBackground()
+        navigationController?.navigationBar.backgroundColor = UIColor.piwigoColorBackground()
+
+        // Table view
+        autoUploadTableView.separatorColor = UIColor.piwigoColorSeparator()
+        autoUploadTableView.indicatorStyle = Model.sharedInstance().isDarkPaletteActive ? .white : .black
+        autoUploadTableView.reloadData()
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+
+        // Set colors, fonts, etc.
+        applyColorPalette()
+
+        // Register palette changes
+        let name: NSNotification.Name = NSNotification.Name(kPiwigoNotificationPaletteChanged)
+        NotificationCenter.default.addObserver(self, selector: #selector(applyColorPalette), name: name, object: nil)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        // Unregister palette changes
+        let name: NSNotification.Name = NSNotification.Name(kPiwigoNotificationPaletteChanged)
+        NotificationCenter.default.removeObserver(self, name: name, object: nil)
+    }
+
+
+    // MARK: - UITableView - Header
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        // Title
+        let titleString = NSLocalizedString("settingsHeader_autoUpload>414px", comment: "Auto Upload Photos")
+        let titleAttributes = [
+            NSAttributedString.Key.font: UIFont.piwigoFontBold()
+        ]
+        let context = NSStringDrawingContext()
+        context.minimumScaleFactor = 1.0
+        let titleRect = titleString.boundingRect(with: CGSize(width: tableView.frame.size.width - 30.0, height: CGFloat.greatestFiniteMagnitude), options: .usesLineFragmentOrigin, attributes: titleAttributes, context: context)
+        return CGFloat(fmax(44.0, titleRect.size.height))
+    }
+
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        // Title
+        let titleString = NSLocalizedString("settingsHeader_autoUpload>414px", comment: "Auto Upload Photos")
+        let titleAttributedString = NSMutableAttributedString(string: titleString)
+        titleAttributedString.addAttribute(.font, value: UIFont.piwigoFontBold(), range: NSRange(location: 0, length: titleString.count))
+
+        // Header label
+        let headerLabel = UILabel()
+        headerLabel.translatesAutoresizingMaskIntoConstraints = false
+        headerLabel.textColor = UIColor.piwigoColorHeader()
+        headerLabel.numberOfLines = 0
+        headerLabel.adjustsFontSizeToFitWidth = false
+        headerLabel.lineBreakMode = .byWordWrapping
+        headerLabel.attributedText = titleAttributedString
+
+        // Header view
+        let header = UIView()
+        header.backgroundColor = UIColor.clear
+        header.addSubview(headerLabel)
+        header.addConstraint(NSLayoutConstraint.constraintView(fromBottom: headerLabel, amount: 4)!)
+        if #available(iOS 11, *) {
+            header.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "|-[header]-|", options: [], metrics: nil, views: [
+            "header": headerLabel
+            ]))
+        } else {
+            header.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "|-15-[header]-15-|", options: [], metrics: nil, views: [
+            "header": headerLabel
+            ]))
+        }
+
+        return header
+    }
+
+
+    // MARK: - UITableView - Rows
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 44.0
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        var tableViewCell = UITableViewCell()
+        
+        switch indexPath.section {
+        case 0:
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: "SwitchTableViewCell", for: indexPath) as? SwitchTableViewCell else {
+                print("Error: tableView.dequeueReusableCell does not return a SwitchTableViewCell!")
+                return SwitchTableViewCell()
+            }
+            let title = NSLocalizedString("settingsHeader_autoUpload", comment: "Auto Upload")
+            cell.configure(with: title)
+            cell.cellSwitch.setOn(Model.sharedInstance().isAutoUploadActive, animated: true)
+            cell.cellSwitchBlock = { switchState in
+                Model.sharedInstance().isAutoUploadActive = switchState
+                Model.sharedInstance().saveToDisk()
+                
+                self.autoUploadTableView.reloadSections(IndexSet.init(integer: indexPath.section), with: .automatic)
+            }
+            tableViewCell = cell
+        default:
+            break
+        }
+        return tableViewCell
+    }
+
+        
+    // MARK: - UITableView - Footer
+    func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
+        // No footer by default (nil => 0 point)
+        var footer = ""
+
+        // Any footer text?
+        switch section {
+        case 0:
+            if Model.sharedInstance().isAutoUploadActive {
+                if Model.sharedInstance().serverFileTypes.contains("mp4") {
+                    footer = NSLocalizedString("settingsFooter_autoUploadEnabledInfo", comment: "Photos and videos will be automatically uploaded to your Piwigo")
+                } else {
+                    footer = NSLocalizedString("settingsFooter_autoUploadEnabledInfo", comment: "Photos will be automatically uploaded to your Piwigo")
+                }
+            } else {
+                footer = NSLocalizedString("settingsFooter_autoUploadDisabledInfo", comment: "Photos will not be automatically uploaded to your Piwigo")
+            }
+        default:
+            return 16.0
+        }
+
+        // Footer height?
+        let attributes = [
+            NSAttributedString.Key.font: UIFont.piwigoFontSmall()
+        ]
+        let context = NSStringDrawingContext()
+        context.minimumScaleFactor = 1.0
+        let footerRect = footer.boundingRect(with: CGSize(width: tableView.frame.size.width - 30.0, height: CGFloat.greatestFiniteMagnitude), options: .usesLineFragmentOrigin, attributes: attributes, context: context)
+
+        return ceil(footerRect.size.height + 10.0)
+    }
+
+    func tableView(_ tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
+        // Footer label
+        let footerLabel = UILabel()
+        footerLabel.translatesAutoresizingMaskIntoConstraints = false
+        footerLabel.font = UIFont.piwigoFontSmall()
+        footerLabel.textColor = UIColor.piwigoColorHeader()
+        footerLabel.textAlignment = .center
+        footerLabel.numberOfLines = 0
+        footerLabel.adjustsFontSizeToFitWidth = false
+        footerLabel.lineBreakMode = .byWordWrapping
+
+        // Any footer text?
+        switch section {
+        case 0:
+            if Model.sharedInstance().isAutoUploadActive {
+                if Model.sharedInstance().serverFileTypes.contains("mp4") {
+                    footerLabel.text = NSLocalizedString("settingsFooter_autoUploadEnabledInfoAll", comment: "Photos and videos will be automatically uploaded to your Piwigo.")
+                } else {
+                    footerLabel.text = NSLocalizedString("settingsFooter_autoUploadEnabledInfo", comment: "Photos will be automatically uploaded to your Piwigo.")
+                }
+            } else {
+                footerLabel.text = NSLocalizedString("settingsFooter_autoUploadDisabledInfo", comment: "Photos will not be automatically uploaded to your Piwigo.")
+            }
+        default:
+            break
+        }
+
+        // Footer view
+        let footer = UIView()
+        footer.backgroundColor = UIColor.clear
+        footer.addSubview(footerLabel)
+        footer.addConstraint(NSLayoutConstraint.constraintView(fromTop: footerLabel, amount: 4)!)
+        if #available(iOS 11, *) {
+            footer.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "|-[footer]-|", options: [], metrics: nil, views: [
+            "footer": footerLabel
+            ]))
+        } else {
+            footer.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "|-15-[footer]-15-|", options: [], metrics: nil, views: [
+            "footer": footerLabel
+            ]))
+        }
+
+        return footer
+    }
+
+
 }
