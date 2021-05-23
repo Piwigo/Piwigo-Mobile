9b8023078cbd26de291e0ccb410178aee760a85f
Eddy Lelièvre-Berna <eddy@lelievre-berna.net>
2021-05-23 09:41:21 +0200

    Moves supporting targets into common folder, prepares shared framework for future extensions

---
+++ Supporting Targets/piwigoKit/piwigoKit.h
@@ -0,0 +1,19 @@
+//
+//  piwigoKit.h
+//  piwigoKit
+//
+//  Created by Eddy Lelièvre-Berna on 22/05/2021.
+//  Copyright © 2021 Piwigo.org. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for piwigoKit.
+FOUNDATION_EXPORT double piwigoKitVersionNumber;
+
+//! Project version string for piwigoKit.
+FOUNDATION_EXPORT const unsigned char piwigoKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <piwigoKit/PublicHeader.h>
+
+
