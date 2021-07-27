bfbc981a94b2a9c8c60873f8a35af0ea0743a672
Eddy Lelièvre-Berna <eddy@lelievre-berna.net>
2021-07-27 23:49:12 +0200

    Adds downsample() method to reduce memory footprint

---
+++ piwigo/Image/ImageUtilities.swift
@@ -0,0 +1,28 @@
+//
+//  ImageUtilities.swift
+//  piwigo
+//
+//  Created by Eddy Lelièvre-Berna on 27/07/2021.
+//  Copyright © 2021 Piwigo.org. All rights reserved.
+//
+
+import Foundation
+import ImageIO
+
+class ImageUtilities: NSObject {
+    
+    // Downsampling large images for display at smaller size (WWDC 2018 - Session 219)
+    class func downsample(imageAt imageURL: URL, to pointSize: CGSize, scale: CGFloat) -> UIImage {
+        let imageSourceOptions = [kCGImageSourceShouldCache: false] as CFDictionary
+        let imageSource = CGImageSourceCreateWithURL(imageURL as CFURL, imageSourceOptions)!
+        
+        let maxDimensionInPixels = max(pointSize.width, pointSize.height) * scale
+        let downsampleOptions = [kCGImageSourceCreateThumbnailFromImageAlways: true,
+                                 kCGImageSourceShouldCacheImmediately: true,
+                                 kCGImageSourceCreateThumbnailWithTransform: true,
+                                 kCGImageSourceThumbnailMaxPixelSize: maxDimensionInPixels] as CFDictionary
+
+        let downsampledImage = CGImageSourceCreateThumbnailAtIndex(imageSource, 0, downsampleOptions)!
+        return UIImage(cgImage: downsampledImage)
+    }
+}
