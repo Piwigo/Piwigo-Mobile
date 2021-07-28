b4406c6baed60254e87a58235695773ae2b6d807
Eddy Lelièvre-Berna <eddy@lelievre-berna.net>
2021-07-29 00:41:38 +0200

    Fixes colour palette not immediately changed

--- piwigo/Settings/SettingsViewController.swift
+++ piwigo/Settings/SettingsViewController.swift
@@ -203,9 +203,7 @@ class SettingsViewController: UIViewController, UITableViewDataSource, UITableVi
         })
     }
 
-    override func viewWillDisappear(_ animated: Bool) {
-        super.viewWillDisappear(animated)
-
+    deinit {
         // Unregister palette changes
         NotificationCenter.default.removeObserver(self, name: PwgNotifications.paletteChanged, object: nil)
         
