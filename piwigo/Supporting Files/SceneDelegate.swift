bcd4cefe183d28835f322aebc461d2c1c826c3c7
Eddy Lelièvre-Berna <eddy@lelievre-berna.net>
2021-10-10 14:10:09 +0200

    Comments for future version

--- piwigo/Supporting Files/SceneDelegate.swift
+++ piwigo/Supporting Files/SceneDelegate.swift
@@ -28,9 +28,11 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        if let userActivity = connectionOptions.userActivities.first {
-            debugPrint(userActivity)
-        }
+        
+        // TO DO when all data will be cached in Core Data database: Scene management 
+//        if let userActivity = connectionOptions.userActivities.first {
+//            debugPrint(userActivity)
+//        }
 
         guard let _ = (scene as? UIWindowScene) else { return }
         if let windowScene = scene as? UIWindowScene {
