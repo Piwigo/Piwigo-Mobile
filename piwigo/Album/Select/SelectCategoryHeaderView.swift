972b89c3cf67dce497f26714b8642d8a0ea60593
Eddy Lelièvre-Berna <eddy@lelievre-berna.net>
2021-10-16 10:34:12 +0200

    Lightens code by removing useless UIColor class name

--- piwigo/Album/Select/SelectCategoryHeaderView.swift
+++ piwigo/Album/Select/SelectCategoryHeaderView.swift
@@ -44,7 +44,7 @@ class SelectCategoryHeaderView: UIView {
         titleRect.size.height += 16
         self.frame = titleRect
         label.text = text
-        label.textColor = UIColor.piwigoColorLeftLabel()
+        label.textColor = .piwigoColorLeftLabel()
     }
 
     required init?(coder aDecoder: NSCoder) {
