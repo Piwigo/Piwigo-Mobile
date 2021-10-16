a85d3de5c4f8906e7e6a21876d5c7563a4da7e04
Eddy Lelièvre-Berna <eddy@lelievre-berna.net>
2021-10-16 11:03:10 +0200

    Makes whole class accessible to Objective-C code

--- piwigo/Supporting Files/Macros/UIFont+AppFonts.swift
+++ piwigo/Supporting Files/Macros/UIFont+AppFonts.swift
@@ -11,67 +11,54 @@ import UIKit
 @objc
 extension UIFont {
 
-    @objc
     class func piwigoFontLight() -> UIFont {
         return UIFont(name: "OpenSans-Light", size: 17.0) ?? UIFont.systemFont(ofSize: 17.0)
     }
 
-    @objc
     class func piwigoFontNormal() -> UIFont {
         return UIFont(name: "OpenSans", size: 17.0) ?? UIFont.systemFont(ofSize: 17.0)
     }
 
-    @objc
     class func piwigoFontSemiBold() -> UIFont {
         return UIFont(name: "OpenSans-Semibold", size: 17.0) ?? UIFont.boldSystemFont(ofSize: 17.0)
     }
 
-    @objc
     class func piwigoFontBold() -> UIFont {
         return UIFont(name: "OpenSans-Bold", size: 17.0) ?? UIFont.boldSystemFont(ofSize: 17.0)
     }
 
-    @objc
     class func piwigoFontExtraBold() -> UIFont {
         return UIFont(name: "OpenSans-Extrabold", size: 17.0) ?? UIFont.boldSystemFont(ofSize: 17.0)
     }
 
-    @objc
     class func piwigoFontSmall() -> UIFont {
         return UIFont(name: "OpenSans", size: 13.0) ?? UIFont.systemFont(ofSize: 13.0)
     }
 
-    @objc
     class func piwigoFontSmallLight() -> UIFont {
         return UIFont(name: "OpenSans-Light", size: 13.0) ?? UIFont.systemFont(ofSize: 13.0)
     }
 
-    @objc
     class func piwigoFontSmallSemiBold() -> UIFont {
         return UIFont(name: "OpenSans-Semibold", size: 13.0) ?? UIFont.boldSystemFont(ofSize: 13.0)
     }
 
-    @objc
     class func piwigoFontTiny() -> UIFont {
         return UIFont(name: "OpenSans", size: 10.0) ?? UIFont.systemFont(ofSize: 10.0)
     }
 
-    @objc
     class func piwigoFontLarge() -> UIFont {
         return UIFont(name: "OpenSans", size: 28.0) ?? UIFont.systemFont(ofSize: 28.0)
     }
 
-    @objc
     class func piwigoFontLargeTitle() -> UIFont {
         return UIFont(name: "OpenSans-Extrabold", size: 28.0) ?? UIFont.boldSystemFont(ofSize: 28.0)
     }
 
-    @objc
     class func piwigoFontButton() -> UIFont {
         return UIFont(name: "OpenSans", size: 21.0) ?? UIFont.systemFont(ofSize: 21.0)
     }
 
-    @objc
     class func fontSizeFor(label: UILabel?, nberLines: Int) -> CGFloat {
         
         if label?.adjustsFontSizeToFitWidth == false || (label?.minimumScaleFactor ?? 1.0) >= 1.0 {
