519cb85a27edc4a80c87ba2722182e22594e3e62
Eddy Lelièvre-Berna <eddy@lelievre-berna.net>
2021-10-16 10:58:47 +0200

    Lightens code by removing useless UIColor class name

--- piwigo/Settings/Information/ReleaseNotesViewController.swift
+++ piwigo/Settings/Information/ReleaseNotesViewController.swift
@@ -35,7 +35,7 @@ class ReleaseNotesViewController: UIViewController {
     @objc
     func applyColorPalette() {
         // Background color of the view
-        view.backgroundColor = UIColor.piwigoColorBackground()
+        view.backgroundColor = .piwigoColorBackground()
 
         // Navigation bar
         let attributes = [
@@ -47,25 +47,25 @@ class ReleaseNotesViewController: UIViewController {
             navigationController?.navigationBar.prefersLargeTitles = false
         }
         navigationController?.navigationBar.barStyle = AppVars.isDarkPaletteActive ? .black : .default
-        navigationController?.navigationBar.tintColor = UIColor.piwigoColorOrange()
-        navigationController?.navigationBar.barTintColor = UIColor.piwigoColorBackground()
-        navigationController?.navigationBar.backgroundColor = UIColor.piwigoColorBackground()
+        navigationController?.navigationBar.tintColor = .piwigoColorOrange()
+        navigationController?.navigationBar.barTintColor = .piwigoColorBackground()
+        navigationController?.navigationBar.backgroundColor = .piwigoColorBackground()
 
         if #available(iOS 15.0, *) {
             /// In iOS 15, UIKit has extended the usage of the scrollEdgeAppearance,
             /// which by default produces a transparent background, to all navigation bars.
             let barAppearance = UINavigationBarAppearance()
             barAppearance.configureWithOpaqueBackground()
-            barAppearance.backgroundColor = UIColor.piwigoColorBackground()
+            barAppearance.backgroundColor = .piwigoColorBackground()
             navigationController?.navigationBar.standardAppearance = barAppearance
             navigationController?.navigationBar.scrollEdgeAppearance = navigationController?.navigationBar.standardAppearance
         }
 
         // Text color depdending on background color
-        authorsLabel.textColor = UIColor.piwigoColorText()
-        versionLabel.textColor = UIColor.piwigoColorText()
-        textView.textColor = UIColor.piwigoColorText()
-        textView.backgroundColor = UIColor.piwigoColorBackground()
+        authorsLabel.textColor = .piwigoColorText()
+        versionLabel.textColor = .piwigoColorText()
+        textView.textColor = .piwigoColorText()
+        textView.backgroundColor = .piwigoColorBackground()
     }
 
     override func viewWillAppear(_ animated: Bool) {
