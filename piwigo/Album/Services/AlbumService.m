f1f5807d463f3cbd284446d245c9976d76cf2959
Eddy Lelièvre-Berna <eddy@lelievre-berna.net>
2022-01-15 15:30:41 +0100

    Checks whether the default album has been deleted from the web UI.

--- piwigo/Album/Services/AlbumService.m
+++ piwigo/Album/Services/AlbumService.m
@@ -118,6 +118,20 @@ NSString * const kCategoryDeletionModeAll = @"force_delete";
                           [UploadUtilitiesObjc disableAutoUpload];
                       }
                       
+                      // Check whether the default album still exists
+                      NSInteger defaultCatId = AlbumVars.defaultCategory;
+                      NSInteger indexOfDefault = [albums indexOfObjectPassingTest:^BOOL(id  _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
+                          PiwigoAlbumData *category = (PiwigoAlbumData *)obj;
+                          if(category.albumId == defaultCatId) {
+                              return YES;
+                          } else {
+                              return NO;
+                          }
+                      }];
+                      if (indexOfDefault != NSNotFound) {
+                          AlbumVars.defaultCategory = 0;    // Back to root album
+                      }
+                      
                       // Update albums if Community extension installed (not needed for admins)
                       if (!NetworkVarsObjc.hasAdminRights &&
                           NetworkVarsObjc.usesCommunityPluginV29)
