3b32548759b0ded29dfa6d8c2f18fca432b0384f
Eddy Lelièvre-Berna <eddy@lelievre-berna.net>
2022-01-23 16:21:04 +0100

    Replaces v2.10.1 release notes with v2.9.2 release notes

--- piwigo/Settings/Information/ReleaseNotesViewController.swift
+++ piwigo/Settings/Information/ReleaseNotesViewController.swift
@@ -164,14 +164,14 @@ class ReleaseNotesViewController: UIViewController {
         let spacerRange = NSRange(location: 0, length: spacerAttributedString.length)
         spacerAttributedString.addAttribute(.font, value: UIFont.piwigoFontTiny(), range: spacerRange)
 
-        // Release 2.10.0 — Bundle string
-        let v2A0String = NSLocalizedString("v2.10.0_text", tableName: "ReleaseNotes", bundle: Bundle.main, value: "", comment: "v2.10.0 Release Notes text")
-        let v2A0AttributedString = NSMutableAttributedString(string: v2A0String)
-        var v2A0Range = NSRange(location: 0, length: v2A0String.count)
-        v2A0AttributedString.addAttribute(.font, value: UIFont.piwigoFontSmall(), range: v2A0Range)
-        v2A0Range = NSRange(location: 0, length: (v2A0String as NSString).range(of: "\n").location)
-        v2A0AttributedString.addAttribute(.font, value: UIFont.piwigoFontBold(), range: v2A0Range)
-        notesAttributedString.append(v2A0AttributedString)
+        // Release 2.9.2 — Bundle string
+        let v292String = NSLocalizedString("v2.9.2_text", tableName: "ReleaseNotes", bundle: Bundle.main, value: "", comment: "v2.9.2 Release Notes text")
+        let v292AttributedString = NSMutableAttributedString(string: v292String)
+        var v292Range = NSRange(location: 0, length: v292String.count)
+        v292AttributedString.addAttribute(.font, value: UIFont.piwigoFontSmall(), range: v292Range)
+        v292Range = NSRange(location: 0, length: (v292String as NSString).range(of: "\n").location)
+        v292AttributedString.addAttribute(.font, value: UIFont.piwigoFontBold(), range: v292Range)
+        notesAttributedString.append(v292AttributedString)
         notesAttributedString.append(spacerAttributedString)
 
         // Release 2.9.1 — Bundle string
