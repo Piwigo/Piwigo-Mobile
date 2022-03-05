1ef003d02f17c99cf05b2b0c1ed42bb5eb3dcf85
Eddy Lelièvre-Berna <eddy@lelievre-berna.net>
2022-03-05 15:23:11 +0100

    Improves HTTP basic authentication management

--- piwigoKit/Network/PwgSession.swift
+++ piwigoKit/Network/PwgSession.swift
@@ -281,23 +281,45 @@ extension PwgSession: URLSessionDataDelegate {
 
         // Check authentication method
         let authMethod = challenge.protectionSpace.authenticationMethod
-        guard authMethod == NSURLAuthenticationMethodHTTPBasic,
-            authMethod == NSURLAuthenticationMethodHTTPDigest else {
+        guard [NSURLAuthenticationMethodHTTPBasic, NSURLAuthenticationMethodHTTPDigest].contains(authMethod) else {
             completionHandler(.performDefaultHandling, nil)
             return
         }
         
+        // Initialise HTTP authentication flag
+        NetworkVars.didFailHTTPauthentication = false
+        
         // Get HTTP basic authentification credentials
         let service = NetworkVars.serverProtocol + NetworkVars.serverPath
-        let account = NetworkVars.httpUsername
-        let password = KeychainUtilities.password(forService: service, account: account)
-        if password.isEmpty {
-            completionHandler(.cancelAuthenticationChallenge, nil)
+        var account = NetworkVars.httpUsername
+        var password = KeychainUtilities.password(forService: service, account: account)
+
+        // Without HTTP credentials available, tries Piwigo credentials
+        if account.isEmpty || password.isEmpty {
+            // Retrieve Piwigo credentials
+            account = NetworkVars.username
+            password = KeychainUtilities.password(forService: NetworkVars.serverPath, account: account)
+            
+            // Adopt Piwigo credentials as HTTP basic authentification credentials
+            NetworkVars.httpUsername = account
+            KeychainUtilities.setPassword(password, forService: service, account: account)
+        }
+
+        // Supply requested credentials if not provided yet
+        if (challenge.previousFailureCount == 0) {
+            // Try HTTP credentials…
+            let credential = URLCredential(user: account,
+                                           password: password,
+                                           persistence: .forSession)
+            completionHandler(.useCredential, credential)
             return
         }
-        let credential = URLCredential(user: account,
-                                       password: password,
-                                       persistence: .forSession)
-        completionHandler(.useCredential, credential)
+
+        // HTTP credentials refused... delete them in Keychain
+        KeychainUtilities.deletePassword(forService: service, account: account)
+
+        // Remember failed HTTP authentication
+        NetworkVars.didFailHTTPauthentication = true
+        completionHandler(.performDefaultHandling, nil)
     }
 }
