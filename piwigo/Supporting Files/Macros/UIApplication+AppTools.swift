8d013ea73a4bfb5e46f3a5412a9a54acd5c59db6
Eddy Lelièvre-Berna <eddy@lelievre-berna.net>
2022-05-07 19:12:32 +0200

    Fixes function not returning all top view controllers

--- piwigo/Supporting Files/Macros/UIApplication+AppTools.swift
+++ piwigo/Supporting Files/Macros/UIApplication+AppTools.swift
@@ -11,18 +11,20 @@ import Foundation
 extension UIApplication {
     
     // MARK: - Top Most View Controllers of an App
-    /// One or several top view controllers if there are several scenes in the foreground.
+    /// One or several top view controllers when there are several scenes in the foreground.
     @objc
     func topViewControllers() -> [UIViewController] {
         var topViewControllers = [UIViewController]()
         
         if #available(iOS 13, *) {
             // Consider only scenes in the foreground and retain the first one
-            let scene = connectedScenes.filter({$0.activationState == .foregroundActive})
-                .compactMap({$0}).first
-            if let windowScene = scene as? UIWindowScene,
-               let topViewController = windowScene.topMostViewController() {
-                topViewControllers.append(topViewController)
+            let activeScenes = connectedScenes.filter({$0.activationState == .foregroundActive})
+                .compactMap({$0})
+            for scene in activeScenes {
+                if let windowScene = scene as? UIWindowScene,
+                   let topViewController = windowScene.topMostViewController() {
+                    topViewControllers.append(topViewController)
+                }
             }
         } else {
             // No scenes -> get the app key window rootViewController
