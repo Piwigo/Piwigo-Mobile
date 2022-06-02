3f2bdddb3a031d00884b43e32ee9a89376242f17
Eddy Lelièvre-Berna <eddy@lelievre-berna.net>
2022-06-02 19:08:21 +0200

    Corrects filename and copyright year

--- piwigoKit/Network/pwg.categories/pwg.categories.setRepresentative.swift
+++ piwigoKit/Network/pwg.categories/pwg.categories.setRepresentative.swift
@@ -3,7 +3,7 @@
 //  piwigoKit
 //
 //  Created by Eddy Lelièvre-Berna on 01/06/2022.
-//  Copyright © 2020 Piwigo.org. All rights reserved.
+//  Copyright © 2022 Piwigo.org. All rights reserved.
 //
 
 import Foundation
