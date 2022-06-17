57a0650f5f128f275c1199d872bdc73f69fd4bd5
Eddy Lelièvre-Berna <eddy@lelievre-berna.net>
2022-06-17 19:12:01 +0200

    Presents "discover" albums with AlbumImagesViewController (prepares #297), suppresses DiscoverImagesViewController

--- piwigo/Data Cache/Categories Data/CategoriesData.m
+++ piwigo/Data Cache/Categories Data/CategoriesData.m
@@ -8,7 +8,6 @@
 
 #import "AlbumData.h"
 #import "CategoriesData.h"
-#import "DiscoverImagesViewController.h"
 #import "SearchImagesViewController.h"
 #import "TaggedImagesViewController.h"
 
@@ -635,11 +634,6 @@ NSString * const kPiwigoNotificationChangedCurrentCategory = @"kPiwigoNotificati
         } else if ([viewController isKindOfClass:[SearchImagesViewController class]]) {
             SearchImagesViewController *vc = (SearchImagesViewController *)viewController;
             [vc removeImageWithId:image.imageId];
-        } else if ([viewController isKindOfClass:[DiscoverImagesViewController class]]) {
-            DiscoverImagesViewController *vc = (DiscoverImagesViewController *)viewController;
-            if (vc.categoryId == category.integerValue) {
-                [vc removeImageWithId:image.imageId];
-            }
         } else if ([viewController isKindOfClass:[TaggedImagesViewController class]]) {
             TaggedImagesViewController *vc = (TaggedImagesViewController *)viewController;
             [vc removeImageWithId:image.imageId];
