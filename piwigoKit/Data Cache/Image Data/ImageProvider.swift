f6cf77a2d1affc305f228ce10feee6e066046824
Eddy Lelièvre-Berna <eddy@lelievre-berna.net>
2023-02-19 22:28:44 +0100

    Adds version 13.4.0 to patch dealing with wrong total number of images

--- piwigoKit/Data Cache/Image Data/ImageProvider.swift
+++ piwigoKit/Data Cache/Image Data/ImageProvider.swift
@@ -175,7 +175,8 @@ public class ImageProvider: NSObject {
                     if albumId == pwgSmartAlbum.favorites.rawValue {
                         totalCount = imageJSON.paging?.count ?? Int64.zero
                     } else {
-                        if "13.5.0".compare(NetworkVars.pwgVersion, options: .numeric) == .orderedSame {
+                        if "13.4.0".compare(NetworkVars.pwgVersion, options: .numeric) == .orderedSame ||
+                            "13.5.0".compare(NetworkVars.pwgVersion, options: .numeric) == .orderedSame {
                             // Bug leading to server providing wrong total_count value
                             // Discovered in Piwigo 13.5.0, may have appeared in earlier version
                             totalCount = imageJSON.paging?.count ?? Int64.zero
