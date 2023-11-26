c4a0efdfa84b00003947b5c8724298023533e4a2
Eddy Lelièvre-Berna <eddy@lelievre-berna.net>
2023-11-26 17:01:53 +0100

    Corrects comment, JSON variable name

---
+++ piwigoKit/Network/pwg.history/PgSession+History.swift
@@ -0,0 +1,9 @@
+//
+//  PgSession+History.swift
+//  piwigoKit
+//
+//  Created by Eddy Lelièvre-Berna on 26/11/2023.
+//  Copyright © 2023 Piwigo.org. All rights reserved.
+//
+
+import Foundation
---
+++ piwigoKit/Network/pwg.history/pwg.history.log.swift
@@ -0,0 +1,63 @@
+//
+//  pwg.history.log.swift
+//  piwigoKit
+//
+//  Created by Eddy Lelièvre-Berna on 26/11/2023.
+//  Copyright © 2023 Piwigo.org. All rights reserved.
+//
+
+import Foundation
+
+// MARK: - pwg.history.log
+public let pwgHistoryLog = "format=json&method=pwg.history.log"
+
+public struct HistoryLogJSON: Decodable {
+
+    public var status: String?
+    public var success = false
+    public var errorCode = 0
+    public var errorMessage = ""
+
+    private enum RootCodingKeys: String, CodingKey {
+        case status = "stat"
+        case errorCode = "err"
+        case errorMessage = "message"
+    }
+    
+    private enum ErrorCodingKeys: String, CodingKey {
+        case code = "code"
+        case message = "msg"
+    }
+
+    public init(from decoder: Decoder) throws
+    {
+        // Root container keyed by RootCodingKeys
+        let rootContainer = try decoder.container(keyedBy: RootCodingKeys.self)
+        
+        // Status returned by Piwigo
+        status = try rootContainer.decodeIfPresent(String.self, forKey: .status)
+        if status == "ok"
+        {
+            success = true
+        }
+        else if status == "fail"
+        {
+            do {
+                // Retrieve Piwigo server error
+                errorCode = try rootContainer.decode(Int.self, forKey: .errorCode)
+                errorMessage = try rootContainer.decode(String.self, forKey: .errorMessage)
+            }
+            catch {
+                // Error container keyed by ErrorCodingKeys ("format=json" forgotten in call)
+                let errorContainer = try rootContainer.nestedContainer(keyedBy: ErrorCodingKeys.self, forKey: .errorCode)
+                errorCode = Int(try errorContainer.decode(String.self, forKey: .code)) ?? NSNotFound
+                errorMessage = try errorContainer.decode(String.self, forKey: .message)
+            }
+        }
+        else {
+            // Unexpected Piwigo server error
+            errorCode = -1
+            errorMessage = NSLocalizedString("serverUnknownError_message", comment: "Unexpected error encountered while calling server method with provided parameters.")
+        }
+    }
+}
