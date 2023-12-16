4b3272ae5765314954d665b9e9ae4a8b1cc75294
Eddy Lelièvre-Berna <eddy@lelievre-berna.net>
2023-12-16 17:37:50 +0100

    Adds version 3.1.2 release notes

--- piwigo/Settings/Information/ReleaseNotesViewController.swift
+++ piwigo/Settings/Information/ReleaseNotesViewController.swift
@@ -156,6 +156,7 @@ class ReleaseNotesViewController: UIViewController {
         let notesAttributedString = NSMutableAttributedString(string: "")
 
         // Release 3.1.x — Bundle string
+        notesAttributedString.append(releaseNotes("v3.1.2_text", comment: "v3.1.2 Release Notes text"))
         notesAttributedString.append(releaseNotes("v3.1.1_text", comment: "v3.1.1 Release Notes text"))
         notesAttributedString.append(releaseNotes("v3.1.0_text", comment: "v3.1.0 Release Notes text"))
 
