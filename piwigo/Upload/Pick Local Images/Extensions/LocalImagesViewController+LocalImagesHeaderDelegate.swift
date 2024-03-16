ae9894b87b733afdb55eda84657c43e2c5145e5a
Eddy Lelièvre-Berna <eddy@lelievre-berna.net>
2024-03-16 12:58:49 +0100

    Renames source file

---
+++ piwigo/Upload/Pick Local Images/Extensions/LocalImagesViewController+LocalImagesHeaderDelegate.swift
@@ -0,0 +1,9 @@
+//
+//  LocalImagesViewController+LocalImagesHeaderDelegate.swift
+//  piwigo
+//
+//  Created by Eddy Lelièvre-Berna on 16/03/2024.
+//  Copyright © 2024 Piwigo.org. All rights reserved.
+//
+
+import Foundation
