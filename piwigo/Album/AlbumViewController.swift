a7c5a07212e5447143776296f9a9934e2ff64066
Eddy Lelièvre-Berna <eddy@lelievre-berna.net>
2024-06-07 21:28:36 +0200

    Displays What's New in Piwigo when the version 3.2 of the app is launched for the first time

--- piwigo/Album/AlbumViewController.swift
+++ piwigo/Album/AlbumViewController.swift
@@ -478,7 +478,7 @@ class AlbumViewController: UIViewController
         /// Next line to be used for dispalying What's New in Piwigo:
         //        AppVars.shared.didShowWhatsNewAppVersion = "3.0.2"
         if let appVersionString = Bundle.main.object(forInfoDictionaryKey: "CFBundleShortVersionString") as? String {
-            if AppVars.shared.didShowWhatsNewAppVersion.compare("3.1", options: .numeric) == .orderedAscending,
+            if AppVars.shared.didShowWhatsNewAppVersion.compare("3.2", options: .numeric) == .orderedAscending,
                appVersionString.compare(AppVars.shared.didShowWhatsNewAppVersion, options: .numeric) == .orderedDescending {
                 // Display What's New in Piwigo
                 let whatsNewSB = UIStoryboard(name: "WhatsNewViewController", bundle: nil)
