a06fafe17f7e523d995e78f88837af3bf8bb6ccc
Eddy Lelièvre-Berna <eddy@lelievre-berna.net>
2024-07-02 22:06:42 +0200

    Adds Troubleshooting section in Settings, updates PwgSession logs

---
+++ piwigo/Settings/Information/LogsViewController.swift
@@ -0,0 +1,159 @@
+//
+//  JsonViewController.swift
+//  piwigo
+//
+//  Created by Eddy Lelièvre-Berna on 30/06/2024.
+//  Copyright © 2024 Piwigo.org. All rights reserved.
+//
+
+import Foundation
+import MessageUI
+import UIKit
+import piwigoKit
+
+class JsonViewController: UIViewController {
+    
+    @IBOutlet weak var method: UILabel!
+    @IBOutlet weak var dateTime: UILabel!
+    @IBOutlet weak var fileContent: UITextView!
+    var fileURL: URL?
+    private var fixTextPositionAfterLoadingViewOnPad: Bool!
+    private var mailBarButton: UIBarButtonItem?
+
+    // MARK: - View Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        title = NSLocalizedString("settings_JSONinvalid", comment: "Invalid JSON data")
+        
+        // Initialise content
+        guard let fileURL = fileURL else { return }
+        let prefixCount = JSONprefix.count
+        let suffixCount = JSONextension.count
+        let fileName = String(fileURL.lastPathComponent.dropFirst(prefixCount).dropLast(suffixCount))
+        if let pos = fileName.lastIndex(of: " ") {
+            method?.text = String(fileName[pos...].dropFirst())
+            dateTime?.text = String(fileName[...pos]) + " | " + fileURL.fileSizeString
+        } else {
+            method?.text = fileName
+            dateTime?.text = fileURL.fileSizeString
+        }
+        let content = try? Data(contentsOf: fileURL, options: .alwaysMapped)
+        fileContent.text = String(decoding: content ?? Data(), as: UTF8.self)
+
+        // Button for sending file content by mail
+        mailBarButton = UIBarButtonItem(barButtonSystemItem: .compose, target: self, action: #selector(mailFile))
+        mailBarButton?.accessibilityIdentifier = "mailFile"
+    }
+    
+    @objc func applyColorPalette() {
+        // Background color of the view
+        view.backgroundColor = .piwigoColorBackground()
+        
+        // Navigation bar
+        let attributes = [
+            NSAttributedString.Key.foregroundColor: UIColor.piwigoColorWhiteCream(),
+            NSAttributedString.Key.font: UIFont.systemFont(ofSize: 17)
+        ]
+        navigationController?.navigationBar.titleTextAttributes = attributes as [NSAttributedString.Key : Any]
+        navigationController?.navigationBar.prefersLargeTitles = false
+        navigationController?.navigationBar.barStyle = AppVars.shared.isDarkPaletteActive ? .black : .default
+        navigationController?.navigationBar.tintColor = .piwigoColorOrange()
+        navigationController?.navigationBar.barTintColor = .piwigoColorBackground()
+        navigationController?.navigationBar.backgroundColor = .piwigoColorBackground()
+        
+        if #available(iOS 15.0, *) {
+            /// In iOS 15, UIKit has extended the usage of the scrollEdgeAppearance,
+            /// which by default produces a transparent background, to all navigation bars.
+            let barAppearance = UINavigationBarAppearance()
+            barAppearance.configureWithOpaqueBackground()
+            barAppearance.backgroundColor = .piwigoColorBackground()
+            navigationController?.navigationBar.standardAppearance = barAppearance
+            navigationController?.navigationBar.scrollEdgeAppearance = navigationController?.navigationBar.standardAppearance
+        }
+        
+        // Text color depdending on background color
+        method.textColor = .piwigoColorText()
+        dateTime.textColor = .piwigoColorText()
+        fileContent.textColor = .piwigoColorText()
+        fileContent.backgroundColor = .piwigoColorBackground()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // Release notes
+        fixTextPositionAfterLoadingViewOnPad = true
+        fileContent.scrollsToTop = true
+        fileContent.contentInsetAdjustmentBehavior = .never
+        
+        // Set colors, fonts, etc.
+        applyColorPalette()
+        
+        // Set navigation buttons
+        navigationItem.setRightBarButtonItems([mailBarButton].compactMap { $0 }, animated: true)
+        
+        // Register palette changes
+        NotificationCenter.default.addObserver(self, selector: #selector(applyColorPalette),
+                                               name: Notification.Name.pwgPaletteChanged, object: nil)
+    }
+    
+    override func viewDidLayoutSubviews() {
+        if (fixTextPositionAfterLoadingViewOnPad) {
+            // Scroll text to where it is expected to be after loading view
+            fixTextPositionAfterLoadingViewOnPad = false
+            fileContent.setContentOffset(.zero, animated: false)
+        }
+    }
+    
+    deinit {
+        // Unregister all observers
+        NotificationCenter.default.removeObserver(self)
+    }
+    
+    
+    // MARK: - Mail File
+    @objc private func mailFile() {
+        if MFMailComposeViewController.canSendMail() {
+            let composeVC = MFMailComposeViewController()
+            composeVC.mailComposeDelegate = self
+
+            // Collect version and build numbers
+            let appVersionString = Bundle.main.object(forInfoDictionaryKey: "CFBundleShortVersionString") as? String
+            let appBuildString = Bundle.main.object(forInfoDictionaryKey: "CFBundleVersion") as? String
+
+            // Set subject
+            var subject = "[" + NSLocalizedString("settings_appName", comment: "Piwigo Mobile") + "]: "
+            subject += method.text ?? "?"
+            composeVC.setSubject(subject)
+
+            // Collect system and device data
+            let deviceModel = UIDevice.current.modelName
+            let deviceOS = UIDevice.current.systemName
+            let deviceOSversion = UIDevice.current.systemVersion
+
+            // Set message body
+            var content = NSLocalizedString("settings_appName", comment: "Piwigo Mobile")
+            content += " " + (appVersionString ?? "") + " (" + (appBuildString ?? "") + ")\n"
+            content += deviceModel + " — " + deviceOS + " " + deviceOSversion + "\n"
+            content += (dateTime.text ?? "?") + "\n"
+            content += "\n"
+            content += fileContent.text ?? ""
+            composeVC.setMessageBody(content, isHTML: false)
+
+            // Present the view controller modally.
+            present(composeVC, animated: true)
+        }
+    }
+}
+
+
+// MARK: - MFMailComposeViewControllerDelegate
+extension JsonViewController: MFMailComposeViewControllerDelegate {
+    func mailComposeController(_ controller: MFMailComposeViewController, didFinishWith result: MFMailComposeResult, error: Error?) {
+        // Check the result or perform other tasks.
+
+        // Dismiss the mail compose view controller.
+        dismiss(animated: true)
+    }
+}
---
+++ piwigo/Settings/Information/TroubleshootingViewController.swift
@@ -0,0 +1,332 @@
+//
+//  LogsViewController.swift
+//  piwigo
+//
+//  Created by Eddy Lelièvre-Berna on 30/06/2024.
+//  Copyright © 2024 Piwigo.org. All rights reserved.
+//
+
+import Foundation
+import OSLog
+import UIKit
+import piwigoKit
+
+@available(iOS 15, *)
+class LogsViewController: UIViewController {
+    
+    @IBOutlet private weak var piwigoLogo: UIImageView!
+    @IBOutlet private weak var authorsLabel: UILabel!
+    @IBOutlet private weak var versionLabel: UILabel!
+    @IBOutlet private weak var tableView: UITableView!
+    
+    private var clearBarButton: UIBarButtonItem?
+    
+    
+    // MARK: - TableView Data
+    private var queue: OperationQueue = OperationQueue()
+    private lazy var fm: FileManager = FileManager.default
+    private lazy var JSONprefixCount: Int = JSONprefix.count
+    private lazy var JSONextensionCount: Int = JSONextension.count
+    private var JSONfiles = [URL]()
+    private var pwgSessionLogs = [OSLogEntryLog]()
+    
+    
+    // MARK: - View Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        title = NSLocalizedString("settings_logs", comment: "Logs")
+        
+        // Button for returning to albums/images
+        clearBarButton = UIBarButtonItem(barButtonSystemItem: .trash, target: self, action: #selector(deleteJSONfiles))
+        clearBarButton?.isEnabled = false
+        clearBarButton?.accessibilityIdentifier = "trash"
+        
+        // Fetch data
+        getLogsAndJSONData()
+    }
+    
+    @objc func applyColorPalette() {
+        // Background color of the view
+        view.backgroundColor = .piwigoColorBackground()
+        
+        // Navigation bar
+        let attributes = [
+            NSAttributedString.Key.foregroundColor: UIColor.piwigoColorWhiteCream(),
+            NSAttributedString.Key.font: UIFont.systemFont(ofSize: 17)
+        ]
+        navigationController?.navigationBar.titleTextAttributes = attributes
+        navigationController?.navigationBar.prefersLargeTitles = false
+        navigationController?.navigationBar.barStyle = AppVars.shared.isDarkPaletteActive ? .black : .default
+        navigationController?.navigationBar.tintColor = .piwigoColorOrange()
+        navigationController?.navigationBar.barTintColor = .piwigoColorBackground()
+        navigationController?.navigationBar.backgroundColor = .piwigoColorBackground()
+        
+        /// In iOS 15, UIKit has extended the usage of the scrollEdgeAppearance,
+        /// which by default produces a transparent background, to all navigation bars.
+        let barAppearance = UINavigationBarAppearance()
+        barAppearance.configureWithOpaqueBackground()
+        barAppearance.backgroundColor = .piwigoColorBackground()
+        navigationController?.navigationBar.standardAppearance = barAppearance
+        navigationController?.navigationBar.scrollEdgeAppearance = navigationController?.navigationBar.standardAppearance
+        
+        // Text color depdending on background color
+        authorsLabel.textColor = .piwigoColorText()
+        versionLabel.textColor = .piwigoColorText()
+        
+        // Table view
+        tableView.separatorColor = .piwigoColorSeparator()
+        tableView.indicatorStyle = AppVars.shared.isDarkPaletteActive ? .white : .black
+        tableView.reloadData()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // Piwigo authors and app version
+        authorsLabel.text = SettingsUtilities.getAuthors(forView: view)
+        versionLabel.text = SettingsUtilities.getAppVersion()
+        
+        // Set colors, fonts, etc.
+        applyColorPalette()
+        
+        // Set navigation buttons
+        navigationItem.setRightBarButtonItems([clearBarButton].compactMap { $0 }, animated: true)
+        
+        // Register palette changes
+        NotificationCenter.default.addObserver(self, selector: #selector(applyColorPalette),
+                                               name: Notification.Name.pwgPaletteChanged, object: nil)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        // Display HUD while retrieving logs and invalid JSON data
+        if queue.operations.count != 0 {
+            navigationController?.showHUD(
+                withTitle: NSLocalizedString("loadingHUD_label", comment: "Loading…"),
+                detail: NSLocalizedString("settings_logs", comment: "Logs"), minWidth: 200)
+        }
+    }
+    
+    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+        super.viewWillTransition(to: size, with: coordinator)
+        
+        // Update Piwigo authors label
+        coordinator.animate(alongsideTransition: { (context) in
+            // Piwigo authors
+            self.authorsLabel.text = SettingsUtilities.getAuthors(forView: self.view)
+        }, completion: nil)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        // Cancel operations (with HUD shown, should never be needed)
+        queue.cancelAllOperations()
+    }
+    
+    deinit {
+        // Unregister all observers
+        NotificationCenter.default.removeObserver(self)
+    }
+    
+    
+    // MARK: - Fetch & Delete Data
+    private func getLogsAndJSONData() {
+        // Operation for retrieving logs
+        let getLogs = BlockOperation {
+            do {
+                let timeCounter = CFAbsoluteTimeGetCurrent()
+                let logStore = try OSLogStore(scope: .currentProcessIdentifier)
+                let oneHourAgo = logStore.position(date: Date().addingTimeInterval(-3600))
+                let predicate = NSPredicate(format: "subsystem IN %@", ["org.piwigo", "org.piwigoKit"])
+                let allEntries = try logStore.getEntries(at: oneHourAgo, matching: predicate)
+                let duration = (CFAbsoluteTimeGetCurrent() - timeCounter)*1000
+                print("••> completed in \(duration.rounded()) ms")
+                self.pwgSessionLogs = allEntries.compactMap { $0 as? OSLogEntryLog }
+            } catch {
+                debugPrint("••> Could not retrieve logs.")
+                self.pwgSessionLogs = []
+            }
+        }
+        getLogs.completionBlock = {
+            DispatchQueue.main.async {
+                self.navigationController?.hideHUD {
+                    self.tableView.reloadSections(IndexSet(integer: 0), with: .automatic)
+                }
+            }
+        }
+
+        // Operation for retrieving invalid JSON data
+        let getJSONfiles = BlockOperation {
+            let tmpURL = self.fm.temporaryDirectory
+            do {
+                let allFiles = try self.fm.contentsOfDirectory(at: tmpURL, includingPropertiesForKeys: [.fileSizeKey],
+                                                               options: .skipsHiddenFiles)
+                self.JSONfiles = allFiles.filter({$0.lastPathComponent.hasPrefix(JSONprefix)})
+                    .sorted(by: {$0.lastPathComponent < $1.lastPathComponent})
+            }
+            catch {
+                debugPrint("!!! Could not retrieve content of temporary directory. !!!")
+                self.JSONfiles =  []
+            }
+        }
+        getJSONfiles.completionBlock = {
+            DispatchQueue.main.async {
+                self.tableView.reloadSections(IndexSet(integer: 1), with: .automatic)
+                self.clearBarButton?.isEnabled = !self.JSONfiles.isEmpty
+            }
+        }
+        
+        // Perform both operations in background and in parallel
+        queue.maxConcurrentOperationCount = .max   // Make it a serial queue for debugging with 1
+        queue.qualityOfService = .userInteractive
+        queue.addOperations([getLogs, getJSONfiles], waitUntilFinished: false)
+    }
+    
+    @objc func deleteJSONfiles() {
+        // Delete JSON data files
+        if JSONfiles.count > 100 {
+            // Show progress view
+            showHUD(withTitle: "")
+            // Remove files
+            JSONfiles.forEach { fileURL in
+                try? fm.removeItem(at: fileURL)
+            }
+            // Hide progress view and reload section
+            hideHUD {
+                self.tableView.reloadSections(IndexSet(integer: 1), with: .automatic)
+                self.clearBarButton?.isEnabled = !self.JSONfiles.isEmpty
+            }
+        } else {
+            // Remove files
+            JSONfiles.forEach { fileURL in
+                try? fm.removeItem(at: fileURL)
+            }
+            // Reload section
+            self.tableView.reloadSections(IndexSet(integer: 1), with: .automatic)
+            self.clearBarButton?.isEnabled = !self.JSONfiles.isEmpty
+        }
+    }
+}
+
+
+// MARK: - UITableViewDataSource Methods
+@available(iOS 15, *)
+extension LogsViewController: UITableViewDataSource
+{
+    // MARK: - Sections
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
+    
+    // MARK: - Rows
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        switch section {
+        case 0 /* Logs */:
+            return 1
+        case 1 /* Invalid JSON data */:
+            return max(1, JSONfiles.count)
+        default:
+            return 0
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .subtitle, reuseIdentifier: "subtitle")
+        
+        switch indexPath.section {
+        case 0 /* Logs */:
+            if pwgSessionLogs.isEmpty {
+                cell.textLabel?.text = "None"
+                cell.accessoryType = UITableViewCell.AccessoryType.none
+            } else {
+                cell.textLabel?.text = "PwgSession"
+                if let date = pwgSessionLogs.first?.date {
+                    cell.detailTextLabel?.text = DateUtilities.dateFormatter.string(from: date) + " | " + String(pwgSessionLogs.count)
+                }
+                cell.accessoryType = UITableViewCell.AccessoryType.disclosureIndicator
+            }
+        case 1 /* Invalid JSON data */:
+            if JSONfiles.isEmpty {
+                cell.textLabel?.text = "None"
+                cell.accessoryType = UITableViewCell.AccessoryType.none
+            } else {
+                let fileURL = JSONfiles[indexPath.row]
+                let fileName = String(fileURL.lastPathComponent.dropFirst(JSONprefixCount).dropLast(JSONextensionCount))
+                if let pos = fileName.lastIndex(of: " ") {
+                    cell.textLabel?.text = String(fileName[pos...].dropFirst())
+                    cell.detailTextLabel?.text = String(fileName[...pos]) + " | " + fileURL.fileSizeString
+                } else {
+                    cell.textLabel?.text = fileName
+                    cell.detailTextLabel?.text = fileURL.fileSizeString
+                }
+                cell.accessoryType = UITableViewCell.AccessoryType.disclosureIndicator
+            }
+        default:
+            break
+        }
+        return cell
+    }
+}
+
+
+// MARK: - UITableViewDelegate Methods
+@available(iOS 15, *)
+extension LogsViewController: UITableViewDelegate
+{
+    // MARK: - Headers
+    private func getContentOfHeader(inSection section: Int) -> (String, String) {
+        // Header strings
+        var title = "", text = ""
+        switch section {
+        case 0 /* Logs */:
+            title = NSLocalizedString("settings_logs", comment: "Logs")
+        case 1 /* Invalid JSON data */:
+            title = NSLocalizedString("settings_JSONinvalid", comment: "Invalid JSON data")
+        default:
+            title = ""
+        }
+        return (title, text)
+    }
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        let (title, text) = getContentOfHeader(inSection: section)
+        if title.isEmpty, text.isEmpty {
+            return CGFloat(1)
+        } else {
+            return TableViewUtilities.shared.heightOfHeader(withTitle: title, text: text,
+                                                            width: tableView.frame.size.width)
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let (title, text) = getContentOfHeader(inSection: section)
+        return TableViewUtilities.shared.viewOfHeader(withTitle: title, text: text)
+    }
+    
+    
+    // MARK: - Cell Management
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        switch indexPath.section {
+        case 0 /* Logs */:
+            guard let JsonVC = storyboard?.instantiateViewController(withIdentifier: "JsonViewController") as? JsonViewController,
+               indexPath.row < pwgSessionLogs.count
+            else { preconditionFailure("Could not load JsonViewController") }
+//            JsonVC.fileURL = pwgSessionLogs[indexPath.row]
+            navigationController?.pushViewController(JsonVC, animated: true)
+
+        case 1 /* Invalid JSON data */:
+            guard let JsonVC = storyboard?.instantiateViewController(withIdentifier: "JsonViewController") as? JsonViewController,
+               indexPath.row < JSONfiles.count
+            else { preconditionFailure("Could not load JsonViewController") }
+            JsonVC.fileURL = JSONfiles[indexPath.row]
+            navigationController?.pushViewController(JsonVC, animated: true)
+
+        default:
+            break
+        }
+    }
+}
