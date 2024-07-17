7df7c4e78c912d2fbabd94e6927c3badfe37bbad
Eddy Lelièvre-Berna <eddy@lelievre-berna.net>
2024-07-17 20:21:43 +0200

    Corrects comment

--- piwigo/Upload/Pick Local Images/Extensions/LocalImagesViewController+Data.swift
+++ piwigo/Upload/Pick Local Images/Extensions/LocalImagesViewController+Data.swift
@@ -56,7 +56,7 @@ extension LocalImagesViewController
         // and index 70588 uploads in about the same if there is no upload request already stored.
         // but index 70588 uploads in 69.1 s if there are already 520 stored upload requests
 
-        // Stop sort is already running
+        // Stop sort already running if any
         queue.cancelAllOperations()
         
         // Pause UploadManager while sorting images
