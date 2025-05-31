00f437e911528c44f437133f7237344b252e92a5
Eddy Lelièvre-Berna <eddy@lelievre-berna.net>
2025-05-31 17:50:37 +0200

    Reorganises UploadSettingsViewController

---
+++ piwigo/Upload/Upload Options/UploadSettingsViewController+UITableViewDelegate.swift
@@ -0,0 +1,76 @@
+//
+//  UploadSettingsViewController+UITableViewDelegate.swift
+//  piwigo
+//
+//  Created by Eddy Lelièvre-Berna on 31/05/2025.
+//  Copyright © 2025 Piwigo.org. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UploadSettingsViewController {
+    
+    // MARK: - Header
+    private func getContentOfHeader() -> (String, String) {
+        let title = String(format: "%@\n", NSLocalizedString("imageUploadHeaderTitle_upload", comment: "Upload Settings"))
+        let text = NSLocalizedString("imageUploadHeaderText_upload", comment: "Please set the upload parameters to apply to the selection of photos/videos")
+        return (title, text)
+    }
+
+    override func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        let (title, text) = getContentOfHeader()
+        return TableViewUtilities.shared.heightOfHeader(withTitle: title, text: text,
+                                                        width: tableView.frame.size.width)
+    }
+
+    override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let (title, text) = getContentOfHeader()
+        return TableViewUtilities.shared.viewOfHeader(withTitle: title, text: text)
+    }
+
+    override func tableView(_ tableView: UITableView, shouldHighlightRowAt indexPath: IndexPath) -> Bool {
+        var row = indexPath.row
+        row += (!resizeImageOnUpload && (row > 1)) ? 2 : 0
+        row += (!compressImageOnUpload && (row > 3)) ? 1 : 0
+        switch row {
+        case 2 /* Upload Photo Size */,
+             3 /* Upload Video Size */,
+             6 /* Rename Filename Before Upload */:
+            return true
+        default:
+            return false
+        }
+    }
+
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        var row = indexPath.row
+        row += (!resizeImageOnUpload && (row > 1)) ? 2 : 0
+        row += (!compressImageOnUpload && (row > 3)) ? 1 : 0
+        switch row {
+        case 2 /* Upload Photo Size */:
+            // Present the Upload Photo Size selector
+            let uploadPhotoSizeSB = UIStoryboard(name: "UploadPhotoSizeViewController", bundle: nil)
+            guard let uploadPhotoSizeVC = uploadPhotoSizeSB.instantiateViewController(withIdentifier: "UploadPhotoSizeViewController") as? UploadPhotoSizeViewController else { return }
+            uploadPhotoSizeVC.delegate = self
+            uploadPhotoSizeVC.photoMaxSize = photoMaxSize
+            navigationController?.pushViewController(uploadPhotoSizeVC, animated: true)
+        case 3 /* Upload Video Size */:
+            // Present the Upload Photo Size selector
+            let uploadVideoSizeSB = UIStoryboard(name: "UploadVideoSizeViewController", bundle: nil)
+            guard let uploadVideoSizeVC = uploadVideoSizeSB.instantiateViewController(withIdentifier: "UploadVideoSizeViewController") as? UploadVideoSizeViewController else { return }
+            uploadVideoSizeVC.delegate = self
+            uploadVideoSizeVC.videoMaxSize = videoMaxSize
+            navigationController?.pushViewController(uploadVideoSizeVC, animated: true)
+        default:
+            break
+        }
+    }
+
+    
+    // MARK: - Footer
+    override func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
+        return 0.0
+    }
+}
