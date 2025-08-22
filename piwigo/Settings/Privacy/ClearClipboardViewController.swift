2c177e8f1f0365d7c6ed8206d1e067cb11091e17
Eddy Lelièvre-Berna <eddy@lelievre-berna.net>
2025-08-22 19:33:23 +0200

    Fixes #610 — Updates appearance on iOS 26

--- piwigo/Settings/Privacy/ClearClipboardViewController.swift
+++ piwigo/Settings/Privacy/ClearClipboardViewController.swift
@@ -195,6 +195,10 @@ extension ClearClipboardViewController: UITableViewDelegate {
     
     
     // MARK: - Rows
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return TableViewUtilities.rowHeight
+    }
+
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
 
