7ab2a690d575d0be84e560107b86ab13eecd8cb0
Eddy Lelièvre-Berna <eddy@lelievre-berna.net>
2025-08-22 19:01:13 +0200

    Fixes #610 — Updates appearance on iOS 26

--- piwigo/Settings/Upload/UploadPhotoSizeViewController.swift
+++ piwigo/Settings/Upload/UploadPhotoSizeViewController.swift
@@ -41,6 +41,9 @@ class UploadPhotoSizeViewController: UIViewController {
         super.viewDidLoad()
 
         title = NSLocalizedString("tabBar_upload", comment: "Upload")
+        if #available(iOS 26.0, *) {
+            navigationItem.attributedTitle = TableViewUtilities.shared.attributedTitle(title)
+        }
     }
 
     @MainActor
@@ -137,7 +140,7 @@ extension UploadPhotoSizeViewController: UITableViewDelegate
     
     // MARK: - Rows
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 44.0
+        return TableViewUtilities.rowHeight
     }
 
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
