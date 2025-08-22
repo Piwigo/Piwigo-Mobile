f793dea1ee5fba3271126d84435b2944285dcb5d
Eddy Lelièvre-Berna <eddy@lelievre-berna.net>
2025-08-22 21:36:38 +0200

    Fixes #610 — Updates appearance on iOS 26

--- piwigo/Settings/Appearance/ColorPaletteViewController.swift
+++ piwigo/Settings/Appearance/ColorPaletteViewController.swift
@@ -9,39 +9,39 @@
 import UIKit
 import piwigoKit
 
-class ColorPaletteViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
-
+class ColorPaletteViewController: UIViewController {
+    
     @IBOutlet var tableView: UITableView!
     
-
-// MARK: - View Lifecycle
-
+    
+    // MARK: - View Lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         title = NSLocalizedString("settingsHeader_appearance", comment: "Appearance")
     }
-
+    
     @MainActor
     @objc func applyColorPalette() {
         // Background color of the view
         view.backgroundColor = PwgColor.background
-
+        
         // Navigation bar appearance
         navigationController?.navigationBar.configAppearance(withLargeTitles: false)
-
+        
         // Table view
         tableView.separatorColor = PwgColor.separator
         tableView.indicatorStyle = AppVars.shared.isDarkPaletteActive ? .white : .black
         tableView.reloadData()
     }
-
+    
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-
+        
         // Set colors, fonts, etc.
         applyColorPalette()
-
+        
         // Register palette changes
         NotificationCenter.default.addObserver(self, selector: #selector(applyColorPalette),
                                                name: Notification.Name.pwgPaletteChanged, object: nil)
@@ -53,47 +53,24 @@ class ColorPaletteViewController: UIViewController, UITableViewDataSource, UITab
         // Back to large titles
         navigationController?.navigationBar.prefersLargeTitles = true
     }
-
+    
     deinit {
         // Unregister all observers
         NotificationCenter.default.removeObserver(self)
     }
+}
 
 
-    // MARK: - UITableView - Header
-    private func getContentOfHeader() -> String {
-        let title = NSLocalizedString("settingsHeader_colorPalette", comment: "Color Palette")
-        return title
-    }
-
-    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
-        let title = getContentOfHeader()
-        return TableViewUtilities.shared.heightOfHeader(withTitle: title,
-                                                        width: tableView.frame.size.width)
-    }
-
-    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
-        let title = getContentOfHeader()
-        return TableViewUtilities.shared.viewOfHeader(withTitle: title)
-    }
-
+// MARK: - UITableViewDataSource Methods
+extension ColorPaletteViewController: UITableViewDataSource {
     
-    // MARK: - UITableView - Rows
+    // MARK: - Sections
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 2
     }
-
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        switch indexPath.row {
-        case 0:
-            return 214.0
-        case 1:
-            return 44.0
-        default:
-            return 0
-        }
-    }
-
+    
+    
+    // MARK: - Rows
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         var tableViewCell = UITableViewCell()
 
@@ -144,5 +121,45 @@ class ColorPaletteViewController: UIViewController, UITableViewDataSource, UITab
         tableViewCell.tintColor = PwgColor.orange
 
         return tableViewCell
+    }
+}
+
+
+// MARK: - UITableViewDelegate Methods
+extension ColorPaletteViewController: UITableViewDelegate {
+    
+    // MARK: - Header
+    private func getContentOfHeader() -> String {
+        let title = NSLocalizedString("settingsHeader_colorPalette", comment: "Color Palette")
+        return title
+    }
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        let title = getContentOfHeader()
+        return TableViewUtilities.shared.heightOfHeader(withTitle: title,
+                                                        width: tableView.frame.size.width)
+    }
+    
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let title = getContentOfHeader()
+        return TableViewUtilities.shared.viewOfHeader(withTitle: title)
+    }
+    
+    
+    // MARK: - Rows
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        var height: CGFloat = 0.0
+        switch indexPath.row {
+        case 0:
+            height = 214.0
+        case 1:
+            height = 44.0
+        default:
+            height = 0
+        }
+        if #available(iOS 26.0, *) {
+            height += TableViewUtilities.rowOffset
+        }
+        return height
     }
 }
