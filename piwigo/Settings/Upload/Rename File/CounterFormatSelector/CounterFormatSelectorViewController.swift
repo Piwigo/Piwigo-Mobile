c4eaa9004ea16455499aeb50f6b90e81d0384115
Eddy Lelièvre-Berna <eddy@lelievre-berna.net>
2025-08-22 19:11:30 +0200

    Fixes #610 — Updates appearance on iOS 26

--- piwigo/Settings/Upload/Rename File/CounterFormatSelector/CounterFormatSelectorViewController.swift
+++ piwigo/Settings/Upload/Rename File/CounterFormatSelector/CounterFormatSelectorViewController.swift
@@ -56,7 +56,10 @@ class CounterFormatSelectorViewController: UIViewController {
         
         // Title, header and example
         title = NSLocalizedString("tabBar_upload", comment: "Upload")
-        
+        if #available(iOS 26.0, *) {
+            navigationItem.attributedTitle = TableViewUtilities.shared.attributedTitle(title)
+        }
+
         // Header
         let headerAttributedString = NSMutableAttributedString(string: "")
         let title = String(format: "%@\n", RenameAction.ActionType.addCounter.name)
