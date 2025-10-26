ce44398cf2dd12bde7937056f91898b2a8512e5f
Eddy Lelièvre-Berna <eddy@lelievre-berna.net>
2025-10-26 10:20:13 +0100

    Prepares and upload screenshots for the App Store

--- Supporting Targets/piwigoScreenshots/piwigoScreenshots.swift
+++ Supporting Targets/piwigoScreenshots/piwigoScreenshots.swift
@@ -22,7 +22,7 @@ final class piwigoScreenshots: XCTestCase {
         DispatchQueue.main.async {
             // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
             let app = XCUIApplication()
-            setupSnapshot(app)
+            setupSnapshot(app, waitForAnimations: false)
             app.launch()
             
             // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
