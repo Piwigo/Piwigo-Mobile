c1539b9dd7a6eb6466c4c889e69274a60db342d8
Eddy Lelièvre-Berna <eddy@lelievre-berna.net>
2025-11-08 17:43:09 +0100

    Moves search bar to the navigation toolbar on iOS 26 and to the navigation bar on previous iOS versions

--- piwigo/Album/TagSelector/TagSelectorViewController+Search.swift
+++ piwigo/Album/TagSelector/TagSelectorViewController+Search.swift
@@ -12,31 +12,26 @@ import UIKit
 // MARK: - Search Images
 extension TagSelectorViewController
 {
-    func initSearchBar() {
-        searchController.obscuresBackgroundDuringPresentation = false
-
-        searchController.searchBar.searchBarStyle = .minimal
-        searchController.searchBar.isTranslucent = false
-        searchController.searchBar.showsSearchResultsButton = false
-        searchController.searchBar.placeholder = NSLocalizedString("tags", comment: "Tags")
-        searchController.searchBar.delegate = self
-        searchController.searchBar.searchTextField.font = UIFont.preferredFont(forTextStyle: .body)
-        searchController.searchBar.searchTextField.adjustsFontSizeToFitWidth = true
-        searchController.searchResultsUpdater = self
-        if #available(iOS 26.0, *) {
-            searchController.searchBar.showsCancelButton = false
-            searchController.hidesNavigationBarDuringPresentation = false
-        } else {
-            searchController.searchBar.showsCancelButton = false
-            searchController.hidesNavigationBarDuringPresentation = true
-        }
+    func initSearchBar(_ searchBar: UISearchBar) {
+        // Initialisation
+        searchBar.searchBarStyle = .minimal
+        searchBar.isTranslucent = true
+        searchBar.showsCancelButton = false
+        searchBar.showsSearchResultsButton = false
+        searchBar.placeholder = NSLocalizedString("tags", comment: "Tags")
         
         // Enable Dynamic Type
-        searchController.searchBar.searchTextField.font = UIFont.preferredFont(forTextStyle: .body)
-        searchController.searchBar.searchTextField.adjustsFontForContentSizeCategory = true
-        
-        // Place the search bar in the header of the tableview
-        tagsTableView.tableHeaderView = searchController.searchBar
+        searchBar.searchTextField.font = UIFont.preferredFont(forTextStyle: .body)
+        searchBar.searchTextField.adjustsFontForContentSizeCategory = true
+    }
+}
+
+
+// MARK: - UISearchBarDelegate Methods
+extension TagSelectorViewController: UISearchBarDelegate
+{
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        updateSearchResults(for: searchController)
     }
 }
 
@@ -45,41 +40,16 @@ extension TagSelectorViewController
 extension TagSelectorViewController: UISearchResultsUpdating
 {
     func updateSearchResults(for searchController: UISearchController) {
-        if let query = searchController.searchBar.text {
-            // Update query
-            searchQuery = query
+        // Update query
+        searchQuery = searchController.searchBar.text ?? ""
 
-            // Do not update content before pushing view in tableView(_:didSelectRowAt:)
-            if searchController.isActive {
-                // Update fetch request predicate
-                fetchRequest.predicate = predicate.withSubstitutionVariables(getQueryVar())
+        // Update fetch request predicate
+        fetchRequest.predicate = predicate.withSubstitutionVariables(getQueryVar())
 
-                // Perform a new fetch
-                try? tags.performFetch()
+        // Perform a new fetch
+        try? tags.performFetch()
 
-                // Shows filtered data
-                tableView.reloadData()
-            }
-        }
-    }
-}
-
-
-// MARK: - UISearchBarDelegate Methods
-extension TagSelectorViewController: UISearchBarDelegate
-{
-    public func searchBarShouldBeginEditing(_ searchBar: UISearchBar) -> Bool {
-        // Animates Cancel button appearance
-        if #unavailable(iOS 26.0) {
-            searchBar.setShowsCancelButton(true, animated: true)
-        }
-        return true
-    }
-    
-    public func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
-        // Animates Cancel button disappearance
-        if #unavailable(iOS 26.0) {
-            searchBar.setShowsCancelButton(false, animated: true)
-        }
+        // Shows filtered data
+        tagsTableView.reloadData()
     }
 }
