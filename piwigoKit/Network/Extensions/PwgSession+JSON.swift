a826512a4e7dcd998d9b0a175b4b267a318812c7
Eddy Lelièvre-Berna <eddy@lelievre-berna.net>
2025-12-06 17:43:49 +0100

    Simplifies request response management

--- piwigoKit/Network/Extensions/PwgSession+JSON.swift
+++ piwigoKit/Network/Extensions/PwgSession+JSON.swift
@@ -38,84 +38,89 @@ extension PwgSession
 
         // Launch the HTTP(S) request
         let task = dataSession.dataTask(with: request) { data, response, error in
-            do {
-                // Return communication error if any
-                if let error = error {
-                    throw error
-                }
-                
-                // No error, check that we received a valid response
-                guard var jsonData = data, let httpResponse = response as? HTTPURLResponse
-                else { throw PwgKitError.invalidResponse }
-                
-                // Valid response, check the absence of HTTP error
-                guard (200...299).contains(httpResponse.statusCode)
-                else { throw PwgKitError.invalidStatusCode(statusCode: httpResponse.statusCode) }
-                
-                // Check that we have received some data
-                guard jsonData.isEmpty == false
-                else { throw PwgKitError.emptyJSONobject }
-                
-                // Data returned, try decoding JSON object
-                do {
-                    let decoder = JSONDecoder()
-                    let pwgData = try decoder.decode(jsonObjectClientExpectsToReceive.self, from: jsonData)
-                    
-                    // Log returned data
-                    let countsOfBytes = httpResponse.allHeaderFields.count * MemoryLayout<Dictionary<String, Any>>.stride + jsonData.count * MemoryLayout<Data>.stride
-#if DEBUG
-                    let dataStr = String(decoding: jsonData.prefix(100), as: UTF8.self) + "…"
-//                    let dataStr = String(decoding: jsonData, as: UTF8.self)
-                    PwgSession.logger.notice("\(method) returned \(countsOfBytes, privacy: .public) bytes: \(dataStr, privacy: .public)")
-#else
-                    PwgSession.logger.notice("\(method) returned \(countsOfBytes, privacy: .public) bytes.")
-#endif
-                    
-                    // Return decoded object
-                    completion(.success(pwgData))
-                }
-                catch let error {
-                    // Log invalid returned data
-#if DEBUG
-                    let dataStr = String(decoding: jsonData, as: UTF8.self)
-                    PwgSession.logger.notice("\(method) returned the invalid JSON data: \(dataStr, privacy: .public)")
-#else
-                    let countsOfBytes = jsonData.count * MemoryLayout<Data>.stride
-                    PwgSession.logger.notice("\(method) returned \(countsOfBytes, privacy: .public) bytes of invalid JSON data.")
-#endif
-                    
-                    // Store invalid JSON data for helping user
-                    jsonData.saveInvalidJSON(for: method)
+            
+            // Communication error?
+            if let error = error as? URLError {
+                completion(.failure(.requestFailed(innerError: error)))
+            }
+            
+            // Valid response?
+            guard var jsonData = data, let httpResponse = response as? HTTPURLResponse
+            else {
+                completion(.failure(.invalidResponse))
+                return
+            }
+            
+            // Absence of HTTP error?
+            guard (200...299).contains(httpResponse.statusCode)
+            else {
+                completion(.failure(.invalidStatusCode(statusCode: httpResponse.statusCode)))
+                return
+            }
+        
+            // Data received?
+            guard jsonData.isEmpty == false
+            else {
+                completion(.failure(.emptyJSONobject))
+                return
+            }
 
+            // Try decoding JSON object
+            let decoder = JSONDecoder()
+            do {
+                let pwgData = try decoder.decode(jsonObjectClientExpectsToReceive.self, from: jsonData)
+                
+                // Log returned data
+                let countsOfBytes = httpResponse.allHeaderFields.count * MemoryLayout<Dictionary<String, Any>>.stride + jsonData.count * MemoryLayout<Data>.stride
+#if DEBUG
+                let dataStr = String(decoding: jsonData.prefix(100), as: UTF8.self) + "…"
+//                    let dataStr = String(decoding: jsonData, as: UTF8.self)
+                PwgSession.logger.notice("\(method) returned \(countsOfBytes, privacy: .public) bytes: \(dataStr, privacy: .public)")
+#else
+                PwgSession.logger.notice("\(method) returned \(countsOfBytes, privacy: .public) bytes.")
+#endif
+                
+                // Return decoded object
+                completion(.success(pwgData))
+            }
+            catch let error {
+                // Log invalid returned data
+#if DEBUG
+                let dataStr = String(decoding: jsonData, as: UTF8.self)
+                PwgSession.logger.notice("\(method) returned the invalid JSON data: \(dataStr, privacy: .public)")
+#else
+                let countsOfBytes = jsonData.count * MemoryLayout<Data>.stride
+                PwgSession.logger.notice("\(method) returned \(countsOfBytes, privacy: .public) bytes of invalid JSON data.")
+#endif
+                
+                // Store invalid JSON data for helping user
+                jsonData.saveInvalidJSON(for: method)
+                
+                // Data filtered, try decoding JSON object
+                do {
                     // Try extracting a JSON object
                     guard jsonData.extractingBalancedBraces()
                     else { throw error }
                     
-                    // Data filtered, try decoding JSON object
-                    do {
-                        let decoder = JSONDecoder()
-                        let decodedObject = try decoder.decode(jsonObjectClientExpectsToReceive.self, from: jsonData)
-                        
-                        // Return decoded object
-                        completion(.success(decodedObject))
-                    }
-                    catch {
-                        // Still invalid JSON data
-                        throw error
-                    }
+                    let decodedObject = try decoder.decode(jsonObjectClientExpectsToReceive.self, from: jsonData)
+                    
+                    // Return decoded object
+                    completion(.success(decodedObject))
                 }
-            }
-            catch let error as DecodingError {
-                completion(.failure(.decodingFailed(innerError: error)))
-            }
-            catch let error as URLError {
-                completion(.failure(.requestFailed(innerError: error)))
-            }
-            catch let error as PwgKitError {
-                completion(.failure(error))
-            }
-            catch {
-                completion(.failure(.otherError(innerError: error)))
+                
+                // Still invalid JSON data
+                catch let error as DecodingError {
+                    completion(.failure(.decodingFailed(innerError: error)))
+                }
+                catch let error as URLError {
+                    completion(.failure(.requestFailed(innerError: error)))
+                }
+                catch let error as PwgKitError {
+                    completion(.failure(error))
+                }
+                catch {
+                    completion(.failure(.otherError(innerError: error)))
+                }
             }
         }
         
