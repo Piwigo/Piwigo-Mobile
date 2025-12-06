39762bd24dc731694b0b5e226fb8ed0331749946
Eddy Lelièvre-Berna <eddy@lelievre-berna.net>
2025-12-06 18:21:52 +0100

    Manages Piwigo errors as PwgKitError for pwg.categories.delete method

--- piwigo/Album/AlbumUtilities.swift
+++ piwigo/Album/AlbumUtilities.swift
@@ -199,12 +199,6 @@ class AlbumUtilities: NSObject {
                                 countOfBytesClientExpectsToReceive: 1000) { result in
             switch result {
             case .success(let pwgData):
-                // Piwigo error?
-                if pwgData.errorCode != 0 {
-                    failure(PwgKitError.pwgError(code: pwgData.errorCode, msg: pwgData.errorMessage))
-                    return
-                }
-                
                 // Successful?
                 if pwgData.success {
                     // Album successfully deleted ▶ Remove category ID from list of recently used albums
