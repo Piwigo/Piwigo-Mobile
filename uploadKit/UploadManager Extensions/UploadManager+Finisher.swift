b848559e14fa7c0e6d9db883d4e6c6c24777b70b
Eddy Lelièvre-Berna <eddy@lelievre-berna.net>
2026-01-11 15:44:22 +0100

    Migrates to Swift 6 with throwing functions for Piwigo 12 minimum

--- uploadKit/UploadManager Extensions/UploadManager+Finisher.swift
+++ uploadKit/UploadManager Extensions/UploadManager+Finisher.swift
@@ -10,109 +10,32 @@ import CoreData
 import Photos
 import piwigoKit
 
+@UploadManagerActor
 extension UploadManager {
     
     // MARK: - Tasks Executed after Uploading
-    func finishTransfer(of upload: Upload) {
-        UploadManager.logger.notice("\(upload.objectID.uriRepresentation().absoluteString) • Finish transfer…")
-
-        // Update upload status
-        isFinishing = true
+    func finishTransferOfUpload(withID uploadID: NSManagedObjectID) async {
+        UploadManagerActor.logger.notice("\(uploadID.uriRepresentation().absoluteString) • Finish transfer…")
+        
+        // Retrieve upload request in context of actor
+        guard let upload = try? self.uploadBckgContext.existingObject(with: uploadID) as? Upload
+        else { return }
         
         // Update state of upload resquest and finish upload
-        upload.setState(.finishing, save: true)
+        upload.setState(.finishing)
+        upload.managedObjectContext?.saveIfNeeded()
         
-        // Work depends on Piwigo server version
-        if "12.0.0".compare(NetworkVars.shared.pwgVersion, options: .numeric) != .orderedDescending {
-            // Uploaded with pwg.images.uploadAsync -> Empty the lounge
-            emptyLounge(for: upload)
-        } else {
-            // Uploaded with pwg.images.upload -> Set image title.
-            setImageParameters(for: upload)
-        }
+        // Uploaded with pwg.images.uploadAsync -> Empty the lounge
+        try? await emptyLounge(for: upload)
+
+        // Update upload request
+        upload.setState(.finished)
+        upload.managedObjectContext?.saveIfNeeded()
+
+        // Update badge, upload queue button, etc.
+        updateNberOfUploadsToComplete()
     }
-
-    func didFinishTransfer() {
-        // Update flag
-        isFinishing = false
-        
-        // Foreground or background task?
-        if UploadVars.shared.isExecutingBGUploadTask {
-            // In background task, launch a transfer if possible
-            if countOfBytesToUpload < maxCountOfBytesToUpload {
-                let prepared = (uploads.fetchedObjects ?? []).filter({$0.state == .prepared})
-                let states: [pwgUploadState] = [.preparingError, .preparingFail,
-                                                .uploadingError, .uploadingFail,
-                                                .finishingError]
-                let failed = (uploads.fetchedObjects ?? []).filter({states.contains($0.state)})
-                if isUploading.count < maxNberOfTransfers,
-                   failed.count < maxNberOfFailedUploads,
-                   let upload = prepared.first {
-                    Task { @UploadManagerActor in
-                        launchTransfer(of: upload)
-                    }
-                }
-            } else {
-                UploadManager.logger.notice("Background task stopped (\(self.countOfBytesToUpload, privacy: .public) bytes transferred)")
-            }
-//        } else if UploadVars.shared.isExecutingBGContinuedUploadTask {
-//            // In continued background task, launch a transfer if possible
-//            let prepared = (uploads.fetchedObjects ?? []).filter({$0.state == .prepared})
-//            let states: [pwgUploadState] = [.preparingError, .preparingFail,
-//                                            .uploadingError, .uploadingFail,
-//                                            .finishingError]
-//            let failed = (uploads.fetchedObjects ?? []).filter({states.contains($0.state)})
-//            if isUploading.count < maxNberOfTransfers,
-//               failed.count < maxNberOfFailedUploads,
-//               let upload = prepared.first {
-//                Task { @UploadManagerActor in
-//                    launchTransfer(of: upload)
-//                }
-//            }
-        } else if !isPreparing, isUploading.count <= maxNberOfTransfers {
-            findNextImageToUpload()
-        }
-    }
-
-
-    // MARK: - Set Image Title
-    /// — Called after having uploaded with pwg.images.upload
-    /// — pwg.images.upload does not allow to set the image title
-    func setImageParameters(for upload: Upload) {
-        UploadManager.logger.notice("\(upload.objectID.uriRepresentation().absoluteString) • setImageParameters()")
-        
-        // Prepare creation date
-        let creationDate = DateUtilities.string(from: upload.creationDate)
-
-        // Prepare parameters for setting the image/video data
-        let tagIDs = String((upload.tags ?? Set<Tag>()).map({"\($0.tagId),"}).reduce("", +).dropLast(1))
-        let paramsDict: [String : Any] = [
-            "image_id"            : "\(NSNumber(value: upload.imageId))",
-            "file"                : upload.fileName,
-            "name"                : upload.imageName.utf8mb3Encoded,
-            "author"              : upload.author.utf8mb3Encoded,
-            "date_creation"       : creationDate,
-            "level"               : "\(NSNumber(value: upload.privacyLevel))",
-            "comment"             : upload.comment.utf8mb3Encoded,
-            "tag_ids"             : tagIDs,
-            "single_value_mode"   : "replace",
-            "multiple_value_mode" : "replace"]
-        
-        // Launch request
-        Task {
-            do {
-                // Set image properties
-                try await JSONManager.shared.setInfos(with: paramsDict)
-
-                // Image successfully uploaded and set
-                self.didFinishTransfer(for: upload, error: nil)
-            }
-            catch let error as PwgKitError {
-                self.didFinishTransfer(for: upload, error: error)
-            }
-        }
-    }
-
+    
 
     // MARK: - Empty Lounge
     /**
@@ -120,49 +43,20 @@ extension UploadManager {
      and one must trigger manually their addition to the database.
      If not, they will be visible after some delay (12 minutes).
      */
-    func emptyLounge(for upload: Upload) {
-        UploadManager.logger.notice("\(upload.objectID.uriRepresentation().absoluteString) • Empty lounge")
+    fileprivate func emptyLounge(for upload: Upload) async throws(PwgKitError) {
+        UploadManagerActor.logger.notice("\(upload.objectID.uriRepresentation().absoluteString) • Empty lounge")
         // Empty lounge without reporting potential error
         guard let user = upload.user else {
             // Should never happen
             // ► The lounge will be emptied later by the server
             // ► Continue upload tasks without returning error
-            self.didFinishTransfer(for: upload, error: nil)
             return
         }
         
-        Task {
-            do {
-                // Check session
-                try await JSONManager.shared.checkSession(ofUserWithID: user.objectID, lastConnected: user.lastUsed)
-                
-                // Empty lounge
-                try await JSONManager.shared.processImages(withIds: upload.imageId, inCategory: upload.category)
-                
-                // Job done
-                self.didFinishTransfer(for: upload, error: nil)
-            }
-            catch {
-                Task { @UploadManagerActor in
-                    // Cannot empty the lounge
-                    // ► The lounge will be emptied later by the app or the server
-                    // ► Continue upload tasks
-                    self.didFinishTransfer(for: upload, error: nil)
-                }
-            }
-        }
-    }
-    
-    private func didFinishTransfer(for upload: Upload, error: PwgKitError?) {
-        // Error?
-        if let error = error {
-            upload.setState(.finishingError, error: error, save: false)
-        } else {
-            upload.setState(.finished, save: false)
-        }
-
-        // Consider next image
-        uploadBckgContext.saveIfNeeded()
-        didFinishTransfer()
+        // Check session
+        try await JSONManager.shared.checkSession(ofUserWithID: user.objectID, lastConnected: user.lastUsed)
+        
+        // Empty lounge
+        try await JSONManager.shared.processImages(withIds: upload.imageId, inCategory: upload.category)
     }
 }
