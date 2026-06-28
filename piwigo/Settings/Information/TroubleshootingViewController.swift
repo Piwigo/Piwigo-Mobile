4eebca297f44d6e74d4c02541059de176f9b37ba
Eddy Lelièvre-Berna <eddy@lelievre-berna.net>
2026-06-28 19:28:05 +0200

    Fixes variable definition for release version

--- piwigo/Settings/Information/TroubleshootingViewController.swift
+++ piwigo/Settings/Information/TroubleshootingViewController.swift
@@ -142,6 +142,8 @@ class TroubleshootingViewController: UIViewController {
                 #if DEBUG
                 var someLogs = entries.filter({$0.category == String(describing: AppMetrics.self)})
                 if someLogs.isEmpty == false { self.pwgLogs.append(someLogs) }
+                #else
+                var someLogs = [OSLogEntryLog]()
                 #endif
                 
                 // PwgKit — Core Data
